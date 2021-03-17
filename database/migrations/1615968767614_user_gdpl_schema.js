82a798e0b34875b129f790d0b7f81c781d178bd6
vadim.gumennyj <vadim.gumennyj@gmail.com>
2021-03-17 14:35:29 +0200

    add linkedin data

--- database/migrations/1615968767614_user_gdpl_schema.js
+++ database/migrations/1615968767614_user_gdpl_schema.js
@@ -9,6 +9,8 @@ class UserGdplSchema extends Schema {
       table.integer('terms_id').unsigned().references('id').inTable('terms')
       table.integer('agreements_id').unsigned().references('id').inTable('agreements')
       table.integer('company_id').unsigned().references('id').inTable('companies')
+      table.string('ldin_id', 100)
+      table.string('ldin_token', 254)
     })
   }
 
@@ -17,6 +19,8 @@ class UserGdplSchema extends Schema {
       table.dropColumn('terms_id')
       table.dropColumn('agreements_id')
       table.dropColumn('company_id')
+      table.dropColumn('ldin_id')
+      table.dropColumn('ldin_token')
     })
   }
 }
