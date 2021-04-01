5ad25e51e7057340bd9f4f4cf9add73f0575b1bb
vadim.gumennyj <vadim.gumennyj@gmail.com>
2021-04-01 11:06:46 +0300

    add device token validation

--- app/Validators/SignInGoogleMobile.js
+++ app/Validators/SignInGoogleMobile.js
@@ -7,7 +7,7 @@ class SignInGoogleMobile {
   static schema = () =>
     yup.object().shape({
       role: yup.number().oneOf([ROLE_USER, ROLE_LANDLORD]),
-      token: yup.string().min(30).max(1000).required(),
+      token: yup.string().min(30).max(1600).required(),
       device_token: yup.string().min(30).max(255),
     })
 }
