d98971cf33ac72fe8040b105e475624b5cd251e6
vadim.gumennyj <vadim.gumennyj@gmail.com>
2021-04-27 15:55:47 +0300

    review Add file validation

--- app/Validators/EstateAddFile.js
+++ app/Validators/EstateAddFile.js
@@ -4,13 +4,13 @@ const yup = require('yup')
 
 const Base = require('./Base')
 const { id } = require('../Libs/schemas.js')
-const { FILE_TYPE_COVER, FILE_TYPE_PLAN, FILE_TYPE_DOC } = require('../constants')
+const { FILE_TYPE_PLAN, FILE_TYPE_DOC, FILE_TYPE_IMAGE } = require('../constants')
 
 class EstateAddFile extends Base {
   static schema = () =>
     yup.object().shape({
       estate_id: id.required(),
-      type: yup.string().oneOf([FILE_TYPE_COVER, FILE_TYPE_PLAN, FILE_TYPE_DOC]).required(),
+      type: yup.string().oneOf([FILE_TYPE_PLAN, FILE_TYPE_DOC, FILE_TYPE_IMAGE]).required(),
     })
 }
 
