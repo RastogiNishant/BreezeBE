c9457f69fe7852bb818dd0ce5131c83049c666b2
vadim.gumennyj <vadim.gumennyj@gmail.com>
2021-08-13 17:45:25 +0300

    Add validation file

---
+++ app/Validators/SignInAppleMobile.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const yup = require('yup')
+const Base = require('./Base')
+const { ROLE_USER, ROLE_LANDLORD } = require('../constants')
+
+class SignInAppleMobile extends Base {
+  static schema = () =>
+    yup.object().shape({
+      role: yup.number().oneOf([ROLE_USER, ROLE_LANDLORD]),
+      token: yup.string().min(30).max(1600).required(),
+      device_token: yup.string().min(30).max(255).required(),
+    })
+}
+
+module.exports = SignInAppleMobile
