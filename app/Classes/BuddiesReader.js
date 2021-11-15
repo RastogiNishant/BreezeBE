5bc58fc865502161a5c61c3d8f80ce398de78be1
wajidtarar <wajid.tarar@gmail.com>
2021-11-15 17:04:44 +0200

    Update BuddiesReader.js

--- app/Classes/BuddiesReader.js
+++ app/Classes/BuddiesReader.js
@@ -67,12 +67,15 @@ class ExcelReader {
     var worksheet = workbook.getWorksheet(1);
     try {
       worksheet.eachRow({ includeEmpty: false }, function(row, rowNumber) {
-        console.log("Row " + rowNumber + " = " + JSON.stringify(row.values));
         if(rowNumber != 1){
           let name = row.values[2]
           let tel = JSON.stringify(row.values[3])
           let email = row.values[4]
-          if (name === 'Name' || email === 'Email' || tel === 'Tel.'){
+          var pattern = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+
+          if (!pattern.test(email.toLowerCase())
+              || name === 'Name' 
+              || email === 'Email' || tel === 'Tel.'){
           } else {
             result.push({'name': name, 'tel' : tel, email : email});
           }
