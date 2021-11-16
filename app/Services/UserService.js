c582c0306d0ccb53e6e3326f51a58270af0e2b2c
wajidtarar <wajid.tarar@gmail.com>
2021-11-16 15:51:51 +0200

    Forgot password changes

--- app/Services/UserService.js
+++ app/Services/UserService.js
@@ -126,14 +126,14 @@ class UserService {
    */
   static async requestSendCodeForgetPassword(email) {
     const code = getHash(3)
+    let user = null;
     try {
       const user = await User.findByOrFail({ email })
+    } catch (error) {      
+      throw new AppException("User with this email does not exist");
+    }
       await DataStorage.setItem(user.id, { code }, "forget_password", {ttl: 3600});
       await MailService.sendcodeForgetPasswordMail(user.email, code);
-    } catch (error) {
-      console.log('AppExceptionAppExceptionAppExceptionAppException email')
-        throw new AppException("User with this email does not exist");
-    }
 
   }
 
