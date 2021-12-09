92b1cbc25447f8e2628c106ebbebf3857812d036
wajidtarar <wajidtarar@users.noreply.github.com>
2021-12-10 00:08:19 +0500

    Merged PR #42 - lang change serliazer and db (#42)

--- app/Serializers/UserSerializer.js
+++ app/Serializers/UserSerializer.js
@@ -7,6 +7,7 @@ const BaseSerializer = require('./BaseSerializer')
 class UserSerializer extends BaseSerializer {
   mergeData(item, options = {}) {
     const { isOwner = false, publicOnly = true , basicFields= false} = options
+    item.lang == 'en' ? item.lang = 'en-US' : null
     if (isOwner) {
       return this._getRowJSON(item)
     }
