3b09305af81b14dabe6192e3ee3fc07cedd243f6
wajidtarar <wajidtarar@users.noreply.github.com>
2021-12-23 21:24:55 +0500

    PR Merged(#68) : Remove time limit visit api (#68)
    * limit condition removed
    
    * slots added

--- app/Services/LandlordService.js
+++ app/Services/LandlordService.js
@@ -4,7 +4,7 @@ class LandlordService {
   /**
    *
    */
-  static async getBookedTimeslots(userId, upcoming ) {
+  static async getBookedTimeslots(userId) {
     return Database.table({ _v: 'visits' })
       .select(
         '_u.firstname',
@@ -25,14 +25,18 @@ class LandlordService {
         '_e.number_floors',
         '_e.cover',
         '_v.estate_id',
-        '_v.user_id'
+        '_v.user_id',
+        '_s.slot_length',
+        '_s.start_at',
+        '_s.end_at',
       )
       .select(Database.raw("'50'::int AS percent"))
-      .select(Database.raw("'10'::int AS slot_length"))
+      // .select(Database.raw("'10'::int AS slot_length"))
       .innerJoin({ _e: 'estates' }, function () {
         this.on('_e.id', '_v.estate_id').onIn('_e.user_id', [userId])
       })
       .innerJoin({ _u: 'users' }, '_u.id', '_v.user_id')
+      .innerJoin({ _s: 'time_slots' }, '_s.estate_id', '_v.estate_id')
       .innerJoin({ _t: 'tenants' }, '_t.user_id', '_u.id')
       .where('_v.date', '>', Database.fn.now())
       .orderBy('_v.date')
