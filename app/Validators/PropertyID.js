939069ee71437b054c7ecd08bdc68a416a65c143
Osman Kanadıkırık <ookndkrk@gmail.com>
2021-12-28 13:01:01 +0300

    fix wrong validator name

--- app/Validators/PropertyID.js
+++ app/Validators/PropertyID.js
@@ -3,11 +3,11 @@
 const yup = require('yup')
 const Base = require('./Base')
 
-class IncomeId extends Base {
+class PropertyId extends Base {
   static schema = () =>
     yup.object().shape({
       id: yup.string().min(5, 'Must be 5 digits'),
     })
 }
 
-module.exports = IncomeId
+module.exports = PropertyId
