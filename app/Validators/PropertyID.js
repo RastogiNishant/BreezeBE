c490c60fb316fbe82e5e459f702814d4fb584ae9
Osman Kanadıkırık <ookndkrk@gmail.com>
2022-01-09 01:32:15 +0300

    fix verify property id validation bug

--- app/Validators/PropertyID.js
+++ app/Validators/PropertyID.js
@@ -6,7 +6,8 @@ const Base = require('./Base')
 class PropertyId extends Base {
   static schema = () =>
     yup.object().shape({
-      id: yup.string().min(5, 'Must be 5 digits'),
+      property_id: yup.string().min(5, 'Must be 5 digits'),
+      estate_id: yup.string().nullable(),
     })
 }
 
