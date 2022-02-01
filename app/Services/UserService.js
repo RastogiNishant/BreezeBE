a9329b5d0ebd1dd8a7b192b5cff20d099e27904e
leadeneur <ookndkrk@gmail.com>
2022-02-01 22:13:00 +0300

    bug fix

--- app/Services/UserService.js
+++ app/Services/UserService.js
@@ -4,8 +4,6 @@
 
 const { FirebaseDynamicLinks } = use('firebase-dynamic-links')
 
-const { FirebaseDynamicLinks } = use('firebase-dynamic-links')
-
 const uuid = require('uuid')
 const moment = require('moment')
 const { get, isArray, isEmpty, uniq, omit } = require('lodash')
