055c7c77e44fa51c9d00bfad6f36155c9aa71624
Yong Yi Wong <yongyi.fullstack@gmail.com>
2022-02-18 18:06:21 -0600

    Added role & description to feature/plan

--- app/Validators/CreateFeature.js
+++ app/Validators/CreateFeature.js
@@ -3,6 +3,10 @@
 const yup = require('yup')
 const Base = require('./Base')
 
+const {
+  ROLE_LANDLORD, ROLE_USER, PLAN_FEATURE_COMMON, PLAN_FEATURE_NEW
+} = require('../constants')
+
 class CreateFeature extends Base {
   static schema = () =>
     yup.object().shape({
@@ -10,7 +14,9 @@ class CreateFeature extends Base {
       description: yup.string(),
       plan_id:yup.number().positive(),
       status: yup.boolean().default(true),
-      prices:yup.number().positive()
+      prices:yup.number().positive(),
+      role_id: yup.number().oneOf([ROLE_LANDLORD, ROLE_USER]),
+      feature_label: yup.string().oneOf([PLAN_FEATURE_COMMON, PLAN_FEATURE_NEW])
     })
 }
 
