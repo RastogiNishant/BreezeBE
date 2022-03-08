e37777d28907d924345651a78f8532fa48f4a159
wajidtarar <wajidtarar@users.noreply.github.com>
2022-03-08 10:37:31 +0200

    Squashed commit of the following:
    commit ff401881f28cc96ffdb9f75a7b17efa7536fe24f
    Author: wajidtarar <wajidtarar@users.noreply.github.com>
    Date:   Tue Mar 8 10:36:42 2022 +0200
    
        Update MailService.js
    
    commit 0fd848d1aaf708005b62fa5efabbf373d53d384f
    Author: wajidtarar <wajidtarar@users.noreply.github.com>
    Date:   Tue Mar 8 09:47:32 2022 +0200
    
        Update MailService.js
    
    commit 2d65e3b815b84f21f15a2163ba4704d2d7a5b8a5
    Author: wajidtarar <wajidtarar@users.noreply.github.com>
    Date:   Tue Mar 8 09:28:59 2022 +0200
    
        Update MailService.js

--- app/Services/MailService.js
+++ app/Services/MailService.js
@@ -111,53 +111,61 @@ class MailService {
 
 
 
-    console.log('language__Lang1:', lang)
-    console.log('Localize__l1', l)
-    console.log('subject__message1:',  l.get('landlord.email_reset.password.subject.message', lang))
+    try {
 
-    const templateId =
-      role === ROLE_LANDLORD
-        ? LANDLORD_EMAIL_TEMPLATE
-        : PROSPECT_EMAIL_TEMPLATE
+      console.log('language__Lang1:', lang)
+      // console.log('Localize__l1', l)
+      console.log('subject__message1:',  l.get('landlord.email_reset.password.subject.message', lang))
 
-    const msg = {
-      to: trim(email),
-      from: FromEmail,
-      templateId: templateId,
-      dynamic_template_data: {
-        subject: l.get('landlord.email_reset.password.subject.message', lang),
-        salutation: l.get('email_signature.salutation.message', lang),
-        intro: l.get('landlord.email_reset.password.intro.message', lang),
-        CTA: l.get('landlord.email_reset.password.CTA.message', lang),
-        link: code,        
-        final: l.get('landlord.email_reset.password.final.message', lang),
-        greeting: l.get('email_signature.greeting.message', lang),
-        company: l.get('email_signature.company.message', lang),
-        position: l.get('email_signature.position.message', lang),        
-        tel: l.get('email_signature.tel.message', lang),
-        email: l.get('email_signature.email.message', lang),
-        address: l.get('email_signature.address.message', lang),
-        website: l.get('email_signature.website.message', lang),
-        tel_val: l.get('tel.customer_service.de.message', lang),
-        email_val: l.get('email.customer_service.de.message', lang),
-        address_val: l.get('address.customer_service.de.message', lang),
-        website_val: l.get('website.customer_service.de.message', lang),
-        download_app: l.get('email_signature.download.app.message', lang),
-        team: l.get('email_signature.team.message', lang),        
-        enviromental_responsibility: l.get('email_signature.enviromental.responsibility.message', lang),
-      },
-    }
+      const templateId =
+        role === ROLE_LANDLORD
+          ? LANDLORD_EMAIL_TEMPLATE
+          : PROSPECT_EMAIL_TEMPLATE
 
-    return sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Reset Email delivery successfully')
-    }, error => {
-      console.log('Reset Email delivery failed', error);
-        if (error.response) {
-        console.error(error.response.body)
+      console.log('templateId:', templateId)
+
+      const msg = {
+        to: trim(email),
+        from: FromEmail,
+        templateId: templateId,
+        dynamic_template_data: {
+          subject: l.get('landlord.email_reset.password.subject.message', lang),
+          salutation: l.get('email_signature.salutation.message', lang),
+          intro: l.get('landlord.email_reset.password.intro.message', lang),
+          CTA: l.get('landlord.email_reset.password.CTA.message', lang),
+          link: code,        
+          final: l.get('landlord.email_reset.password.final.message', lang),
+          greeting: l.get('email_signature.greeting.message', lang),
+          company: l.get('email_signature.company.message', lang),
+          position: l.get('email_signature.position.message', lang),        
+          tel: l.get('email_signature.tel.message', lang),
+          email: l.get('email_signature.email.message', lang),
+          address: l.get('email_signature.address.message', lang),
+          website: l.get('email_signature.website.message', lang),
+          tel_val: l.get('tel.customer_service.de.message', lang),
+          email_val: l.get('email.customer_service.de.message', lang),
+          address_val: l.get('address.customer_service.de.message', lang),
+          website_val: l.get('website.customer_service.de.message', lang),
+          download_app: l.get('email_signature.download.app.message', lang),
+          team: l.get('email_signature.team.message', lang),        
+          enviromental_responsibility: l.get('email_signature.enviromental.responsibility.message', lang),
+        },
       }
-    });
+
+      return sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Reset Email delivery successfully')
+      }, error => {
+        console.log('Reset Email delivery failed', error);
+          if (error.response) {
+          console.error(error.response.body)
+        }
+      });
+    } catch (e) {
+      console.log('CATCH sendcodeForgotPasswordMail')
+      console.log('e:', e)
+    }
   }
 
   static async sendcodeForMemberInvitation(email, shortLink) {
