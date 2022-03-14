c4f92ffd016f76e4bc1cef15ba5990cc40f67812
Justin Villocido <barudo@gmail.com>
2022-03-14 19:05:00 +0800

    add middleware to check if Landlord owns estate

---
+++ app/Middleware/LandlordOwnsThisEstate.js
@@ -0,0 +1,28 @@
+'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Estate = use('App/Models/Estate')
+const HttpException = use('App/Exceptions/HttpException')
+const {ERROR_LANDLORD_DOES_NOT_OWN_THIS_ESTATE} = require('../constants')
+
+class LandlordOwnsThisEstate {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle ({ request, auth }, next) {
+    // call next to advance the request
+    console.log('checking if landlord owns this estate.')
+    const {estate_id} = request.all()
+    const landlordId = auth.user.id 
+    const result = await Estate.query().where('user_id', landlordId).where('id', estate_id).first()
+    if(!result) {
+      throw new HttpException('Landlord does not own this estate', 412, ERROR_LANDLORD_DOES_NOT_OWN_THIS_ESTATE)
+    }
+    await next()
+  }
+}
+
+module.exports = LandlordOwnsThisEstate
