295e76ef03bfd2ab624bb235f89f6008b48a0852
Justin Villocido <barudo@gmail.com>
2022-03-19 11:14:01 +0800

    fix race conflict when doing next()

--- app/Middleware/UserCanValidlyChangeEmail.js
+++ app/Middleware/UserCanValidlyChangeEmail.js
@@ -13,26 +13,26 @@ class UserCanValidlyChangeEmail {
    * @param {Request} ctx.request
    * @param {Function} next
    */
-  async handle ({ request, auth, response}, next) {
+  async handle ({ request, auth}, next) {
     //let this pass if email is not set
     if(!request.body.email) {
       await next()
+    } else {
+      //lets find a user having this role and email
+      let user = await User.query().where('email', request.body.email).where('role', auth.user.role).first()
+      if(user) {
+        throw new HttpException('This email already exists in our users. Please use another one.')
+      }
+      //validate password
+      const authenticator = getAuthByRole(auth, auth.user.role)
+      const uid = await User.getHash(auth.user.email, auth.user.role)
+      try {
+        await authenticator.attempt(uid, request.body.password)
+      } catch (e) {
+        throw new HttpException(e, 412, ERROR_CHANGE_EMAIL_PASSWORD_NOT_MATCH)
+      }
+      await next()
     }
-    //lets find a user having this role and email
-    let user = await User.query().where('email', request.body.email).where('role', auth.user.role).first()
-    if(user) {
-      throw new HttpException('This email already exists in our users. Please use another one.')
-    }
-    //validate password
-    const authenticator = getAuthByRole(auth, auth.user.role)
-    const uid = await User.getHash(auth.user.email, auth.user.role)
-    try {
-      await authenticator.attempt(uid, request.body.password)
-    } catch (e) {
-      throw new HttpException(e, 412, ERROR_CHANGE_EMAIL_PASSWORD_NOT_MATCH)
-    }
-    
-    await next()
   }
 }
 
