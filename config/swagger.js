5d32bdcfb1a7a4d7fd5da44f73a21f2f3423daaf
Justin Villocido <barudo@gmail.com>
2022-03-24 20:21:52 +0800

    working on accountController

--- config/swagger.js
+++ config/swagger.js
@@ -8,33 +8,50 @@ module.exports = {
     swaggerDefinition: {
       info: {
         title: 'Breeze Backend API',
+        description: 'Breeze backend API documentation.',
         version: '2.0.0',
       },
-      openapi:'3.0.0',
+      openapi: '3.0.0',
       basePath: '/',
+      servers: [
+        {
+          url: 'http://localhost:3000',
+          description: 'Development server',
+        },
+        {
+          url: 'https://api-dev.breeze4me.com',
+          description: 'Staging Server',
+        },
+        {
+          url: 'https://api.breeze4me.com',
+          description: 'Production Server',
+        },
+      ],
       tags: [
         {
           name: 'Accounts',
-          description: 'All about authentication, information about Landlords, Tenants/Prospects, etc.'
+          description:
+            'All about authentication, information about Landlords, Tenants/Prospects, etc.',
         },
         {
           name: 'Landlords',
-          description: 'All about the Landlord User'
-        }
+          description: 'All about the Landlord User',
+        },
+        {
+          name: 'Prospects',
+          description: 'All about the prospects',
+        },
       ],
       components: {
         securitySchemes: {
           bearerAuth: {
             type: 'http',
             scheme: 'bearer',
-            bearerFormat: 'JWT'
-          }
-        }
+            bearerFormat: 'JWT',
+          },
+        },
       },
     },
-    apis: [
-      'docs/**/*.yml',
-      'start/routes.js',
-    ],
+    apis: ['docs/**/*.yml', 'start/routes.js'],
   },
 }
