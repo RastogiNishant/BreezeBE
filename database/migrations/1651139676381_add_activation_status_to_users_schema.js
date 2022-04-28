5fad3dd122d696205987bba16c8ce4d48afe57d0
Justin Villocido <barudo@gmail.com>
2022-04-28 18:34:34 +0800

    update migration to use integer instead of enu

--- database/migrations/1651139676381_add_activation_status_to_users_schema.js
+++ database/migrations/1651139676381_add_activation_status_to_users_schema.js
@@ -2,23 +2,12 @@
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
-const {
-  USER_ACTIVATION_STATUS_NOT_ACTIVATED,
-  USER_ACTIVATION_STATUS_ACTIVATED,
-  USER_ACTIVATION_STATUS_DEACTIVATED,
-} = require('../../app/constants')
 
 class AddActivationStatusToUsersSchema extends Schema {
   up() {
     this.table('users', (table) => {
       // alter table
-      table
-        .enum('activation_status', [
-          USER_ACTIVATION_STATUS_NOT_ACTIVATED,
-          USER_ACTIVATION_STATUS_ACTIVATED,
-          USER_ACTIVATION_STATUS_DEACTIVATED,
-        ])
-        .comment('activation status')
+      table.integer('activation_status').nullable().default(null).comment('activation status')
     })
   }
 
