4324a0f61350463d8c7ae5595891c55943ae2c68
Yong Yi Wong <yongyi.fullstack@gmail.com>
2022-05-02 08:56:38 -0500

    Added compressing image functionality

--- app/Controllers/Http/RoomController.js
+++ app/Controllers/Http/RoomController.js
@@ -17,7 +17,8 @@ const EstateService = use('App/Services/EstateService')
 const {
   PROPERTY_MANAGE_ALLOWED,
   ROLE_LANDLORD,
-  ROLE_PROPERTY_MANAGER
+  ROLE_PROPERTY_MANAGER,
+  SUPPORTED_IMAGE_SIZE,
 } = require('../../constants')
 const ImageService = require('../../Services/ImageService')
 class RoomController {
@@ -26,33 +27,32 @@ class RoomController {
    */
   async createRoom({ request, auth, response }) {
     const { estate_id, ...roomData } = request.all()
-    try{
-
+    try {
       let userIds = [auth.user.id]
-      if( auth.user.role === ROLE_PROPERTY_MANAGER ) {
-        userIds = await EstatePermissionService.getLandlordIds(auth.user.id, PROPERTY_MANAGE_ALLOWED)      
+      if (auth.user.role === ROLE_PROPERTY_MANAGER) {
+        userIds = await EstatePermissionService.getLandlordIds(
+          auth.user.id,
+          PROPERTY_MANAGE_ALLOWED
+        )
       }
 
-      await Estate.query()
-        .where('id', estate_id)
-        .whereIn('user_id', userIds)
-        .firstOrFail()
-      
-      if( roomData.favorite ) {
+      await Estate.query().where('id', estate_id).whereIn('user_id', userIds).firstOrFail()
+
+      if (roomData.favorite) {
         await Room.query()
           .where('estate_id', estate_id)
           .where('type', roomData.type)
-          .update({'favorite':false })
+          .update({ favorite: false })
       }
       const room = await Room.createItem({
         ...roomData,
         estate_id,
       })
       Event.fire('estate::update', estate_id)
-  
+
       response.res(room)
-    }catch(e) {
-      Logger.error('Create Room error', e);
+    } catch (e) {
+      Logger.error('Create Room error', e)
       throw new HttpException(e.message, 400)
     }
   }
@@ -65,8 +65,8 @@ class RoomController {
     // Get room and check estate owner
 
     let userIds = [auth.user.id]
-    if( auth.user.role === ROLE_PROPERTY_MANAGER ) {
-      userIds = await EstatePermissionService.getLandlordIds(auth.user.id, PROPERTY_MANAGE_ALLOWED)      
+    if (auth.user.role === ROLE_PROPERTY_MANAGER) {
+      userIds = await EstatePermissionService.getLandlordIds(auth.user.id, PROPERTY_MANAGE_ALLOWED)
     }
 
     const room = await RoomService.getRoomByUser(userIds, room_id)
@@ -74,12 +74,12 @@ class RoomController {
       throw new HttpException('Invalid room', 404)
     }
 
-    if( data.favorite ) {
+    if (data.favorite) {
       await Room.query()
         .where('estate_id', estate_id)
-        .where('type', data.type)        
-        .update({'favorite':false })
-    }    
+        .where('type', data.type)
+        .update({ favorite: false })
+    }
     room.merge(data)
     await room.save()
     Event.fire('estate::update', estate_id)
@@ -102,36 +102,36 @@ class RoomController {
     response.res(true)
   }
 
-  async updateOrder({request, auth, response}) {
-    const {ids} = request.all()
+  async updateOrder({ request, auth, response }) {
+    const { ids } = request.all()
     const roomIds = await RoomService.getRoomIds(auth.user.id, ids)
-    if( roomIds.length != ids.length  ) {
-      throw new HttpException('Some roomids don\'t exist')
+    if (roomIds.length != ids.length) {
+      throw new HttpException("Some roomids don't exist")
     }
 
     await Promise.all(
-      ids.map(async(id, index) => {
+      ids.map(async (id, index) => {
         await Room.query()
-        .where('id', id)
-        .update({order:index+1})
+          .where('id', id)
+          .update({ order: index + 1 })
       })
     )
     response.res(true)
   }
 
-  async orderRoomPhoto({request, auth, response}) {
+  async orderRoomPhoto({ request, auth, response }) {
     const { room_id, ids } = request.all()
     const room = await RoomService.getRoomByUser(auth.user.id, room_id)
     if (!room) {
       throw new HttpException('Invalid room', 404)
     }
-    try{
+    try {
       const imageIds = await ImageService.getImageIds(room_id, ids)
-      if( imageIds.length != ids.length ) {
-        throw new HttpException('Some imageIds don\'t exist')        
+      if (imageIds.length != ids.length) {
+        throw new HttpException("Some imageIds don't exist")
       }
       await ImageService.updateOrder(ids)
-    }catch(e){
+    } catch (e) {
       throw new HttpException(e.message, 400)
     }
     response.res(true)
@@ -143,8 +143,8 @@ class RoomController {
     const { room_id } = request.all()
 
     let userIds = [auth.user.id]
-		if (auth.user.role === ROLE_PROPERTY_MANAGER) {
-      userIds = await EstatePermissionService.getLandlordIds(auth.user.id, PROPERTY_MANAGE_ALLOWED)            
+    if (auth.user.role === ROLE_PROPERTY_MANAGER) {
+      userIds = await EstatePermissionService.getLandlordIds(auth.user.id, PROPERTY_MANAGE_ALLOWED)
     }
 
     const room = await RoomService.getRoomByUser(userIds, room_id)
@@ -152,25 +152,47 @@ class RoomController {
       throw new HttpException('Invalid room', 404)
     }
 
-    const image = request.file('file')
+    try {
+      const image = request.file('file', {
+        size: process.env.MAX_IMAGE_SIZE || '20M',
+        extnames: SUPPORTED_IMAGE_SIZE,
+      })
+      if (image.hasErrors) {
+        throw new HttpException(image.errors, 400)
+      }
+      const imagemin = (await import('imagemin')).default
+      const imageminMozjpeg = (await import('imagemin-mozjpeg')).default
+      const imageminPngquant = require('imagemin-pngquant')
 
+      let img_data = Drive.getStream(image.tmpPath)
+      if (image.size > 20000) {
+        // image size is bigger than 2M, it's only for test, we need to change it later
+        img_data = (
+          await imagemin([image.tmpPath], {
+            plugins: [imageminPngquant({ quality: [0.6, 0.8] }), imageminMozjpeg({ quality: 80 })],
+          })
+        )[0].data
+      }
 
-    const ext = image.extname
-      ? image.extname
-      : image.clientName.toLowerCase().replace(/.*(jpeg|jpg|png)$/, '$1')
-    const filename = `${uuid.v4()}.${ext}`
-    const filePathName = `${moment().format('YYYYMM')}/${filename}`
-
-    await Drive.disk('s3public').put(filePathName, Drive.getStream(image.tmpPath), {
-      ACL: 'public-read',
-      ContentType: image.headers['content-type'],
-    })
-    const imageObj = await RoomService.addImage(filePathName, room, 's3public')
-    if (!room.cover) {
-      await EstateService.setCover(room.estate_id, filePathName)
+      const ext = image.extname
+        ? image.extname
+        : image.clientName.toLowerCase().replace(/.*(jpeg|jpg|png)$/, '$1')
+      const filename = `${uuid.v4()}.${ext}`
+      const filePathName = `${moment().format('YYYYMM')}/${filename}`
+      await Drive.disk('s3public').put(filePathName, img_data, {
+        ACL: 'public-read',
+        ContentType: image.headers['content-type'],
+      })
+      const imageObj = await RoomService.addImage(filePathName, room, 's3public')
+      if (!room.cover) {
+        await EstateService.setCover(room.estate_id, filePathName)
+      }
+      Event.fire('estate::update', room.estate_id)
+      response.res(imageObj)
+    } catch (e) {
+      console.log('RoomController Add Image', e)
+      throw new HttpException('Uploading image failure')
     }
-    Event.fire('estate::update', room.estate_id)
-    response.res(imageObj)
   }
 
   /**
