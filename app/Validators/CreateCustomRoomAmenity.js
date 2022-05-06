2cbcfef4988ca16638e478b0d9798229abeb58a1
Justin Villocido <barudo@gmail.com>
2022-05-06 08:42:36 +0800

    add validator for create custom room amenity

--- app/Validators/CreateCustomRoomAmenity.js
+++ app/Validators/CreateCustomRoomAmenity.js
@@ -2,11 +2,11 @@
 const Base = require('./Base')
 const yup = require('yup')
 
-class CreateCustomRoomAmenities extends Base {
+class CreateCustomRoomAmenity extends Base {
   static schema = () =>
     yup.object().shape({
       amenity: yup.string().min(3).max(22).required('custom amenity name is required.'),
     })
 }
 
-module.exports = CreateCustomRoomAmenities
+module.exports = CreateCustomRoomAmenity
