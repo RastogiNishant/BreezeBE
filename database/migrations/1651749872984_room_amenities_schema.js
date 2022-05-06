a575f8638886ebf460508b490b74324e96096033
Justin Villocido <barudo@gmail.com>
2022-05-06 15:59:25 +0800

    ranem migration to room_amenities

--- database/migrations/1651749872984_room_amenities_schema.js
+++ database/migrations/1651749872984_room_amenities_schema.js
@@ -5,16 +5,17 @@ const { STATUS_ACTIVE } = require('../../app/constants')
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
-class RoomCustomAmenitiesSchema extends Schema {
+class RoomAmenitiesSchema extends Schema {
   up() {
     this.create('room_custom_amenities', (table) => {
       table.increments()
-      table.timestamps()
       table.integer('room_id').references('id').inTable('rooms')
+      table.integer('options_id').references('id').inTable('options').nullable()
       table.integer('status').defaultTo(STATUS_ACTIVE).index()
-      table.string('amenity', 22)
+      table.string('amenity', 22).nullable()
       table.integer('sequence_order')
       table.integer('added_by').references('id').inTable('users')
+      table.timestamps()
     })
   }
 
@@ -23,4 +24,4 @@ class RoomCustomAmenitiesSchema extends Schema {
   }
 }
 
-module.exports = RoomCustomAmenitiesSchema
+module.exports = RoomAmenitiesSchema
