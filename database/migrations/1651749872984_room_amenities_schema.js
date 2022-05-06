51c7e8da63e7382e68b27f8016f49c959a22707f
Justin Villocido <barudo@gmail.com>
2022-05-06 16:34:59 +0800

    add migration to migrate options to room_amenities table

--- database/migrations/1651749872984_room_amenities_schema.js
+++ database/migrations/1651749872984_room_amenities_schema.js
@@ -7,20 +7,21 @@ const Schema = use('Schema')
 
 class RoomAmenitiesSchema extends Schema {
   up() {
-    this.create('room_custom_amenities', (table) => {
+    this.create('room_amenities', (table) => {
       table.increments()
       table.integer('room_id').references('id').inTable('rooms')
-      table.integer('options_id').references('id').inTable('options').nullable()
+      table.integer('option_id').references('id').inTable('options').nullable()
       table.integer('status').defaultTo(STATUS_ACTIVE).index()
       table.string('amenity', 22).nullable()
+      table.enu('type', ['amenity', 'custom_amenity'])
       table.integer('sequence_order')
-      table.integer('added_by').references('id').inTable('users')
+      table.integer('added_by').references('id').inTable('users').nullable()
       table.timestamps()
     })
   }
 
   down() {
-    this.drop('room_custom_amenities')
+    this.drop('room_amenities')
   }
 }
 
