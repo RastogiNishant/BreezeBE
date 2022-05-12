8632a2e1ffe59648cd3957a8063947a995b1a98d
Justin Villocido <barudo@gmail.com>
2022-05-12 16:51:57 +0800

    add comment on relations remove secondaryMembers relation

--- app/Models/Tenant.js
+++ app/Models/Tenant.js
@@ -153,14 +153,9 @@ class Tenant extends Model {
     return toCoord(this.coord_raw, false)
   }
 
-  /**
-   *
-   */
-  secondaryMembers() {
-    return this.hasMany('App/Models/Member', 'user_id', 'owner_user_id')
-  }
-
   primaryMember() {
+    //the actual logic is that a primaryMember is a member with members.user_id
+    //is set and equal to the tenant's user_id and email is NULL
     return this.hasOne('App/Models/Member', 'user_id', 'user_id')
   }
 
