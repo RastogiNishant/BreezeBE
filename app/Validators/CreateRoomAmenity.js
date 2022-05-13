cff60b9678655a1836af03a6382d2f9cfd22fbf9
Justin Villocido <barudo@gmail.com>
2022-05-13 10:34:45 +0800

    add max length test to amenity to keep it at most 22 chars

--- app/Validators/CreateRoomAmenity.js
+++ app/Validators/CreateRoomAmenity.js
@@ -1,14 +1,22 @@
 'use strict'
 const Base = require('./Base')
 const yup = require('yup')
+const { ROOM_CUSTOM_AMENITY_MAX_STRING_LENGTH } = require('../constants')
 
 class CreateRoomAmenity extends Base {
   static schema = () =>
     yup.object().shape({
       type: yup.string().oneOf(['amenity', 'custom_amenity']),
       amenity: yup.string().when('type', {
-        is: 'custom-amenity',
-        then: yup.string().min(3).max(22).required('custom amenity name is required.'),
+        is: 'custom_amenity',
+        then: yup
+          .string()
+          .test(
+            'len',
+            `Custom amenity must be less than or equal to ${ROOM_CUSTOM_AMENITY_MAX_STRING_LENGTH} characters`,
+            (val) => val.length <= ROOM_CUSTOM_AMENITY_MAX_STRING_LENGTH
+          )
+          .required('custom amenity name is required.'),
       }),
       option_id: yup.number().when('type', {
         is: 'amenity',
