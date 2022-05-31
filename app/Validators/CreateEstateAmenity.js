16c8a25d227f23f643f7f49931da306f900ca028
Justin Villocido <barudo@gmail.com>
2022-05-31 18:53:45 +0800

    add validator for create EstateAmenity and :location

---
+++ app/Validators/CreateEstateAmenity.js
@@ -0,0 +1,34 @@
+'use strict'
+const Base = require('./Base')
+const yup = require('yup')
+const {
+  ESTATE_CUSTOM_AMENITY_MAX_STRING_LENGTH,
+  ESTATE_AMENITY_LOCATIONS,
+} = require('../constants')
+
+class CreateEstateAmenity extends Base {
+  static schema = () =>
+    yup.object().shape({
+      type: yup.string().oneOf(['amenity', 'custom_amenity']),
+      amenity: yup.string().when('type', {
+        is: 'custom_amenity',
+        then: yup
+          .string()
+          .test(
+            'len',
+            `Custom amenity must be less than or equal to ${ESTATE_CUSTOM_AMENITY_MAX_STRING_LENGTH} characters`,
+            (val) => val.length >= 1 && val.length <= ESTATE_CUSTOM_AMENITY_MAX_STRING_LENGTH
+          )
+          .typeError('Custom amenity must have at least 1 character.')
+          .required('Custom amenity name is required.'),
+      }),
+      option_id: yup.number().when('type', {
+        is: 'amenity',
+        then: yup.number().integer().positive().required('Estate amenity is required.'),
+      }),
+      estate_id: yup.number().integer().positive().required('Estate Id is required'),
+      location: yup.string().oneOf(ESTATE_AMENITY_LOCATIONS),
+    })
+}
+
+module.exports = CreateEstateAmenity
