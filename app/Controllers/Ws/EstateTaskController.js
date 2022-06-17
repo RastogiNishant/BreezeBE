617ec59ce2a71601e8b72dfc4e463336fd39f11a
Justin Villocido <barudo@gmail.com>
2022-06-17 20:17:44 +0800

    placeholders for ws controllers

--- app/Controllers/Ws/EstateTaskController.js
+++ app/Controllers/Ws/EstateTaskController.js
@@ -4,9 +4,16 @@ class EstateTaskController {
   constructor({ socket, request }) {
     this.socket = socket
     this.request = request
+    this.topic = Ws.getChannel('chat:*').topic(this.socket.topic)
   }
 
-  onCreateTask() {}
+  onCreateTask() {
+    //
+  }
+
+  onAnswer() {}
+
+  onMessage() {}
 }
 
 module.exports = EstateTaskController
