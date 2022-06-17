8911c4679f61196e1ca0e9fe7aaae535190e5acb
Justin Villocido <barudo@gmail.com>
2022-06-17 20:25:11 +0800

    placeholders

--- app/Controllers/Ws/EstateChatController.js
+++ app/Controllers/Ws/EstateChatController.js
@@ -5,12 +5,17 @@ class EstateChatController {
   constructor({ socket, request }) {
     this.socket = socket
     this.request = request
-    
+    this.topic = Ws.getChannel('estate:*').topic(this.socket.topic)
   }
 
   onMessage(message) {
     //add record to db...
+  }
 
+  onMarkLastRead
+
+  onConnect() {
+    //send
   }
 }
 
