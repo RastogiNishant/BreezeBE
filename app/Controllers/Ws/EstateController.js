a70170e9597f203a983c3f288f09bfbfd97fa3c8
Justin Villocido <barudo@gmail.com>
2022-07-19 16:50:04 +0800

    removed test codes

--- app/Controllers/Ws/EstateController.js
+++ app/Controllers/Ws/EstateController.js
@@ -3,6 +3,11 @@
 const BaseController = require('./BaseController')
 
 //endpoint for Estate
+//we'll retain this for now. We'll be using this on notifications.
+//ie. once landlord/tenant went through the create task flow, he needs to send
+//an event here like taskCreated. This will enable all those listening to this
+//topic to act accordingly and do something like connect to this topic and start
+//messaging there.
 class EstateController extends BaseController {
   constructor({ socket, auth, request }) {
     super({ socket, auth, request, channel: 'estate' })
@@ -15,8 +20,8 @@ class EstateController extends BaseController {
     //await super._saveToChats(message, this.user.id, this.estate_id)
   }
 
-  async onCreateTask() {
-    super.emitToSender({ task_id: 1 }, 'taskCreated', 0)
+  async onTaskCreated(message) {
+    //we'll populate this later on
   }
 }
 
