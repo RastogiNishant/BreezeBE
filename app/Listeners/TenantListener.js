c7634459e2bbffc9e3452d4d45e42fc80ff07f54
Justin Villocido <barudo@gmail.com>
2022-07-20 17:48:47 +0800

    removed TenantListener.js

--- app/Listeners/TenantListener.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const TenantService = use('App/Services/TenantService')
-
-const TenantListener = (exports = module.exports = {})
-
-TenantListener.updateTenant = async (userId) => {
-  await TenantService.deactivateTenant(userId)
-}
