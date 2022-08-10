729310997583bb08711fde550ac2e48283cb5b70
Justin Villocido <barudo@gmail.com>
2022-08-10 17:05:18 +0800

    add floor_direction on validation

--- app/Validators/ImportEstate.js
+++ app/Validators/ImportEstate.js
@@ -188,6 +188,10 @@ const {
   LETTING_STATUS_FIRST_TIME_USE,
   LETTING_STATUS_VACANCY,
   PARKING_SPACE_TYPE_NO_PARKING,
+
+  ESTATE_FLOOR_DIRECTION_LEFT,
+  ESTATE_FLOOR_DIRECTION_RIGHT,
+  ESTATE_FLOOR_DIRECTION_STRAIGHT,
 } = require('../constants')
 
 yup.addMethod(yup.number, 'mustNotBeSet', function mustNotBeSet() {
@@ -252,6 +256,14 @@ class ImportEstate extends Base {
       house_number: yup.string().min(1).max(255).required('House Number is required.'),
       country: yup.string().min(1).max(255).required('Country is required.'),
       floor: yup.number().integer().min(-10).max(200),
+      floor_direction: yup
+        .number()
+        .integer()
+        .oneOf([
+          ESTATE_FLOOR_DIRECTION_LEFT,
+          ESTATE_FLOOR_DIRECTION_RIGHT,
+          ESTATE_FLOOR_DIRECTION_STRAIGHT,
+        ]),
       number_floors: yup.number().integer().min(1).max(100),
       prices: yup.number().min(0).max(100000),
       net_rent: yup.number().min(0).max(100000),
