83aa72aeb3e2a3bbb29658aff3706624fb230a19
Justin Villocido <barudo@gmail.com>
2022-08-18 22:50:52 +0800

    update admin estate query

--- app/Controllers/Http/Admin/PropertyController.js
+++ app/Controllers/Http/Admin/PropertyController.js
@@ -1,9 +1,23 @@
 'use strict'
+const Database = use('Database')
+const { STATUS_ACTIVE, STATUS_DRAFT } = require('../../../constants')
+
 const Estate = use('App/Models/Estate')
 
 class PropertyController {
   async getProperties({ request, response }) {
-    return response.res(true)
+    let estates = await Estate.query()
+      .select(Database.raw('estates.*'))
+      .select(Database.raw('json_agg(users) as user'))
+      .whereIn('estates.status', [STATUS_ACTIVE, STATUS_DRAFT])
+      .innerJoin('users', 'estates.user_id', 'users.id')
+      /*.with('matches')
+      .with('visits')*/
+      .groupBy('estates.id')
+      .orderBy('estates.updated_at', 'desc')
+      .fetch()
+    estates = estates.toJSON()
+    return response.res(estates)
   }
 }
 
