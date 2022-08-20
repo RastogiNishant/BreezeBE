55061189c39027189511c54f9c23c257dbc8985f
Justin Villocido <barudo@gmail.com>
2022-08-21 07:23:47 +0800

    testing on queries on property controller

--- app/Controllers/Http/Admin/PropertyController.js
+++ app/Controllers/Http/Admin/PropertyController.js
@@ -1,7 +1,13 @@
 'use strict'
 const Database = use('Database')
 const moment = require('moment')
-const { STATUS_ACTIVE, STATUS_DRAFT, STATUS_EXPIRE, DAY_FORMAT } = require('../../../constants')
+const {
+  STATUS_ACTIVE,
+  STATUS_DRAFT,
+  STATUS_EXPIRE,
+  DAY_FORMAT,
+  MATCH_STATUS_INVITE,
+} = require('../../../constants')
 
 const Estate = use('App/Models/Estate')
 
@@ -12,7 +18,8 @@ class PropertyController {
     let estates = await Estate.query()
       .select(Database.raw('estates.*'))
       .select(Database.raw('json_agg(users) as user'))
-      .select(Database.raw('COUNT(visits)::int as visitCount'))
+      .select(Database.raw('COUNT(visits)::int as visit_count'))
+      .select(Database.raw('COUNT(_i.*)::int as invite_count'))
       .whereIn('estates.status', [STATUS_ACTIVE, STATUS_DRAFT, STATUS_EXPIRE])
       .innerJoin('users', 'estates.user_id', 'users.id')
       .leftJoin('time_slots', function () {
@@ -23,6 +30,9 @@ class PropertyController {
         this.on('visits.start_date', '>=', 'time_slots.start_at')
           .on('visits.end_date', '<=', 'time_slots.end_at')
           .on('visits.estate_id', '=', 'estates.id')
+      })
+      .leftJoin(Database.raw(`(select * from matches) as _i`), function () {
+        this.on('_i.estate_id', 'estates.id').on('_i.status', MATCH_STATUS_INVITE)
       })
       .groupBy('time_slots.id', 'estates.id')
       .orderBy('estates.updated_at', 'desc')
