0d8556b414b96a3e3ed9aeb2c999be7a10530f2c
Justin Villocido <barudo@gmail.com>
2022-08-22 07:26:45 +0800

    include visit_count, final_match count, invite counts for estates

--- app/Controllers/Http/Admin/PropertyController.js
+++ app/Controllers/Http/Admin/PropertyController.js
@@ -7,38 +7,87 @@ const {
   STATUS_EXPIRE,
   DAY_FORMAT,
   MATCH_STATUS_INVITE,
+  MATCH_STATUS_FINISH,
 } = require('../../../constants')
 
 const Estate = use('App/Models/Estate')
 
 class PropertyController {
   async getProperties({ request, response }) {
-    const currentDay = moment().startOf('day')
+    const currentDayFormatted = moment().startOf('day').format(DAY_FORMAT)
 
     let estates = await Estate.query()
       .select(Database.raw('estates.*'))
-      .select(Database.raw('json_agg(users) as user'))
-      .select(Database.raw('COUNT(visits)::int as visit_count'))
-      .select(Database.raw('COUNT(_i.*)::int as invite_count'))
+      .select(Database.raw('_u.user'))
+      .select(Database.raw('coalesce(_v.visit_count, 0) as visit_count'))
+      .select(Database.raw('coalesce(_i.invite_count, 0) as invite_count'))
+      .select(Database.raw('coalesce(_f.match_count, 0) as final_match_count'))
       .whereIn('estates.status', [STATUS_ACTIVE, STATUS_DRAFT, STATUS_EXPIRE])
-      .innerJoin('users', 'estates.user_id', 'users.id')
-      .leftJoin('time_slots', function () {
-        this.on('estates.id', 'time_slots.estate_id')
-      })
-      .where('time_slots.end_at', '<=', currentDay.format(DAY_FORMAT))
-      .leftJoin('visits', function () {
-        this.on('visits.start_date', '>=', 'time_slots.start_at')
-          .on('visits.end_date', '<=', 'time_slots.end_at')
-          .on('visits.estate_id', '=', 'estates.id')
-      })
-      .leftJoin(Database.raw(`(select * from matches) as _i`), function () {
-        this.on('_i.estate_id', 'estates.id').on('_i.status', MATCH_STATUS_INVITE)
-      })
-      .groupBy('time_slots.id', 'estates.id')
+      //owner
+      .innerJoin(
+        Database.raw(`(select
+          jsonb_build_object('firstname', users.firstname, 'secondname', users.secondname, 'email', users.email) as user,
+          users.id as user_id
+        from
+          users
+        ) as _u`),
+        'estates.user_id',
+        '_u.user_id'
+      )
+      //visits
+      .leftJoin(
+        Database.raw(`(select
+          COUNT(visits)::int as visit_count,
+          visits.estate_id
+        from
+          visits
+        left join time_slots
+        on time_slots.estate_id=visits.estate_id
+        and visits.start_date >=time_slots.start_at
+        and visits.end_date <= time_slots.end_at
+        and time_slots.end_at <= '${currentDayFormatted}'
+        group by
+        visits.estate_id
+        ) as _v`),
+        'estates.id',
+        '_v.estate_id'
+      )
+      //invites
+      .leftJoin(
+        Database.raw(`(select count(*) as invite_count, estate_id
+        from matches
+        WHERE status=${MATCH_STATUS_INVITE}
+        group by estate_id
+        ) as _i`),
+        '_i.estate_id',
+        'estates.id'
+      )
+      //final matches
+      .leftJoin(
+        Database.raw(`(select count(*) as match_count, estate_id
+        from matches
+        WHERE status=${MATCH_STATUS_FINISH}
+        group by estate_id
+        ) as _f`),
+        '_f.estate_id',
+        'estates.id'
+      )
       .orderBy('estates.updated_at', 'desc')
       .fetch()
     estates = estates.toJSON()
     return response.res(estates)
+  }
+
+  async updatePropertyPublishStatus({ request, response }) {
+    const { ids, action } = request.all()
+    const trx = await Database.beginTransaction()
+    switch (action) {
+      case 'publish':
+
+      case 'unpublish':
+    }
+    await trx.rollback()
+    return response.res(true)
   }
 }
 
