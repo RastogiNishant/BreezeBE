8634568736f1798673a0be946cbc1fbce66fa099
Yong Yi Wong <yongyi.fullstack@gmail.com>
2022-08-30 01:58:26 +0800

    fixed Company Address format

--- app/Models/User.js
+++ app/Models/User.js
@@ -121,7 +121,7 @@ class User extends Model {
    *
    */
   company() {
-    return this.hasMany('App/Models/Company', 'id', 'user_id')
+    return this.belongsTo('App/Models/Company', 'company_id', 'id')
   }
 
   /**
