3a9f46b5d5a088f00ba4fc6e805d3c00ce2c1178
Justin Villocido <barudo@gmail.com>
2022-08-31 10:23:16 +0800

    interchange fetch and where clause

--- database/migrations/1661851083012_adjust_company_schema.js
+++ database/migrations/1661851083012_adjust_company_schema.js
@@ -10,41 +10,51 @@ const Contact = use('App/Models/Contact')
 
 class AdjustCompanySchema extends Schema {
   async up() {
+    let users = await User.query().whereNotNull('company_id').fetch()
 
-    let users = await User.query().fetch().whereNotNull('company_id')
-
-    await Promise.all(users.toJSON().map(async (user) => {
-      const company = await Company.query().where('id', user.company_id).whereNot('status', STATUS_DELETE).first()
-      if (!company) {
-        await User.query().update({ company_id: null }).where('id', user.id)
-      }
-    }))
+    await Promise.all(
+      users.toJSON().map(async (user) => {
+        const company = await Company.query()
+          .where('id', user.company_id)
+          .whereNot('status', STATUS_DELETE)
+          .first()
+        if (!company) {
+          await User.query().update({ company_id: null }).where('id', user.id)
+        }
+      })
+    )
 
     const companies = await Company.query().whereNot('status', STATUS_DELETE).fetch()
 
-    await Promise.all(companies.toJSON().map(async (company) => {
-      if (company.user_id) {
-        await User.query().update({ company_id: company.id }).where('id', company.user_id).whereNull('company_id')
-      }
-    }))
+    await Promise.all(
+      companies.toJSON().map(async (company) => {
+        if (company.user_id) {
+          await User.query()
+            .update({ company_id: company.id })
+            .where('id', company.user_id)
+            .whereNull('company_id')
+        }
+      })
+    )
 
     users = (await User.query().fetch()).toJSON()
 
     const contacts = await Contact.query().fetch()
-    await Promise.all(contacts.toJSON().map(async (contact) => {
-      const user = users.find(user => user.id === contact.user_id && user.company_id)
-      if (user) {
-        await Contact.query().where('user_id', contact.user_id).update({ company_id: user.company_id })
-      } else {
-        await Contact.query().where('user_id', contact.user_id).update({ status: STATUS_DELETE })
-      }
-    }))
-
-
+    await Promise.all(
+      contacts.toJSON().map(async (contact) => {
+        const user = users.find((user) => user.id === contact.user_id && user.company_id)
+        if (user) {
+          await Contact.query()
+            .where('user_id', contact.user_id)
+            .update({ company_id: user.company_id })
+        } else {
+          await Contact.query().where('user_id', contact.user_id).update({ status: STATUS_DELETE })
+        }
+      })
+    )
   }
 
-  down() {
-  }
+  down() {}
 }
 
 module.exports = AdjustCompanySchema
