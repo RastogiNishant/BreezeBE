5ba6330fbab8389eb5ae54848e318c1c19c3fb9a
Yong Yi Wong <yongyi.fullstack@gmail.com>
2022-10-31 15:08:02 +0000

    added title in contact

--- app/Validators/UpdateContact.js
+++ app/Validators/UpdateContact.js
@@ -4,11 +4,13 @@ const yup = require('yup')
 
 const Base = require('./Base')
 const { phoneSchema } = require('../Libs/schemas.js')
+const { SALUTATION_MR_LABEL, SALUTATION_MS_LABEL } = require('../constants')
 
 class UpdateContact extends Base {
   static schema = () => {
     return yup.object().shape({
       email: yup.string().email().lowercase().max(255),
+      title: yup.string().oneOf([SALUTATION_MR_LABEL, SALUTATION_MS_LABEL]),
       full_name: yup.string().min(2).max(255),
       phone: phoneSchema,
       region: yup.string().max(255),
