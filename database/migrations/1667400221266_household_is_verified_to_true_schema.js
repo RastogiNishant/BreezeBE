ce41ba681abcf132782cf178645cecec6a975791
Yong Yi Wong <yongyi.fullstack@gmail.com>
2022-11-02 15:56:58 +0000

    Added filter

--- database/migrations/1667400221266_household_is_verified_to_true_schema.js
+++ database/migrations/1667400221266_household_is_verified_to_true_schema.js
@@ -6,7 +6,10 @@ const Member = use('App/Models/Member')
 
 class HouseholdIsVerifiedToTrueSchema extends Schema {
   async up() {
-    await Member.query().whereNull('owner_user_id').update({ is_verified: true })
+    await Member.query()
+      .whereNotNull('user_id')
+      .whereNull('owner_user_id')
+      .update({ is_verified: true })
   }
 
   down() {}
