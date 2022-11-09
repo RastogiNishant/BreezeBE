bf2ed6ac685311510c935d2a688a0a41a1f4e531
leadeneur <ookndkrk@gmail.com>
2022-11-09 03:57:23 +0300

    add beforeUpdate hook to correct estate statuses by letting_type

--- app/Models/Estate.js
+++ app/Models/Estate.js
@@ -42,10 +42,8 @@ const {
   TASK_STATUS_INPROGRESS,
   TASK_STATUS_DELETE,
   TASK_STATUS_DRAFT,
-  TASK_STATUS_RESOLVED,
-  DATE_FORMAT,
-  TASK_RESOLVE_HISTORY_PERIOD,
-  ROLE_LANDLORD,
+  LETTING_TYPE_LET,
+  STATUS_DRAFT,
 } = require('../constants')
 
 class Estate extends Model {
@@ -213,6 +211,13 @@ class Estate extends Model {
   static boot() {
     super.boot()
     this.addTrait('@provider:SerializerExtender')
+
+    this.addHook('beforeUpdate', async (instance) => {
+      if (instance.letting_type === LETTING_TYPE_LET && instance.status !== STATUS_DRAFT) {
+        instance.status = STATUS_DRAFT
+      }
+    })
+
     this.addHook('beforeSave', async (instance) => {
       if (instance.dirty.coord && isString(instance.dirty.coord)) {
         const [lat, lon] = instance.dirty.coord.split(',')
