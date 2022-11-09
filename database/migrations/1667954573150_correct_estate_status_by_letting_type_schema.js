781890641c6d0787c24beaf6be3275b198ea2697
leadeneur <ookndkrk@gmail.com>
2022-11-09 11:04:30 +0300

    fix missing delete status on migration

--- database/migrations/1667954573150_correct_estate_status_by_letting_type_schema.js
+++ database/migrations/1667954573150_correct_estate_status_by_letting_type_schema.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { STATUS_DRAFT, LETTING_TYPE_LET } = require('../../app/constants')
+const { STATUS_DRAFT, LETTING_TYPE_LET, STATUS_DELETE } = require('../../app/constants')
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
@@ -8,10 +8,10 @@ const Estate = use('App/Models/Estate')
 
 class CorrectEstateStatusByLettingTypeSchema extends Schema {
   up() {
-    this.table('estates', async (table) => {
+    this.table('estates', async () => {
       await Estate.query()
         .update({ status: STATUS_DRAFT })
-        .whereNot('status', STATUS_DRAFT)
+        .whereNotIn('status', [STATUS_DRAFT, STATUS_DELETE])
         .where('letting_type', LETTING_TYPE_LET)
     })
   }
