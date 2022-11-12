eb550cfc7ad28dbf26e08fd1d437f7762f961df8
Yong Yi Wong <yongyi.fullstack@gmail.com>
2022-11-12 11:01:16 +0000

    send estate create notify email to admin  performance

--- app/Controllers/Http/EstateController.js
+++ app/Controllers/Http/EstateController.js
@@ -105,7 +105,7 @@ class EstateController {
           process.env.NODE_ENV || 'local'
         } environment`
 
-        await MailService.sendUnverifiedLandlordActivationEmailToAdmin(txt)
+        MailService.sendUnverifiedLandlordActivationEmailToAdmin(txt)
       }
 
       response.res(estate)
