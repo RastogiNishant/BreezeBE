7106c9f380b00c27be8fd449cdf6ea99640a50d3
Justin Villocido <barudo@gmail.com>
2022-11-30 14:32:56 +0800

    add unit test to getIpBasedInfo

--- app/Libs/getIpBasedInfo.js
+++ app/Libs/getIpBasedInfo.js
@@ -1,13 +1,13 @@
 const axios = require('axios')
-const { isSet, pick } = require('lodash')
+const { has, pick } = require('lodash')
 const ipAddressJs = require('ipaddr.js')
 
-const getIpBasedInfo = async (ip) => {
+const getIpBasedInfo = async (ip, accessKey = null) => {
   if (!isPublicIp(ip)) {
     return false
   }
   const ipstackEndpoint = 'http://api.ipstack.com'
-  const accessKey = process.env.IPSTACK_API_ACCESS_KEY
+  accessKey = accessKey || process.env.IPSTACK_API_ACCESS_KEY
   if (!accessKey) {
     console.error('IPSTACK_API_ACCESS_KEY must be set in .env')
     return false
@@ -15,12 +15,10 @@ const getIpBasedInfo = async (ip) => {
   const url = `${ipstackEndpoint}/${ip}?access_key=${accessKey}`
   try {
     let { data } = await axios.get(url)
-    if (!isSet(data.success)) {
-      //data.success:false is only set when error occurs
+    if (!has(data, 'error')) {
       data = pick(data, ['country_code', 'country_name', 'city', 'zip', 'latitude', 'longitude'])
       return data
     }
-    console.error('ipstack error', data.error)
     return false
   } catch (err) {
     console.error('ipstack error', err.message)
