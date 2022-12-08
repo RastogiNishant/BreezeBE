6c79271c0e4c4ecc50030872839c7c1ba2b2b169
Justin Villocido <barudo@gmail.com>
2022-12-08 15:51:09 +0800

    working on outputting properties from xml import

--- app/Classes/OpenImmoReader.js
+++ app/Classes/OpenImmoReader.js
@@ -1,129 +1,214 @@
-const xmlParser = require('xml2js').Parser()
+const xml2js = require('xml2js')
 const AppException = use('App/Exceptions/AppException')
 const fs = require('fs')
-const { isArray, forOwn } = require('lodash')
+const extract = require('extract-zip')
+const { has, includes, isArray, isNumber, forOwn, get } = require('lodash')
+const { OPENIMMO_EXTRACT_FOLDER } = use('App/constants')
 
 class OpenImmoReader {
   json = {}
-  constructor(filePath, field) {
-    const data = fs.readFileSync(filePath)
+  contentType
+  filePath
+
+  constructor(filePath, contentType) {
+    this.contentType = contentType
+    this.filePath = filePath
+    this.openImmoDefinition = require('../../resources/openimmo/openimmo-127b.json')
+  }
+
+  generateRandomString(length = 6) {
+    let randomString = ''
+    var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    for (var i = 0; i < length; i++) {
+      randomString += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return randomString
+  }
+
+  extractJson(xml) {
     const that = this
-    xmlParser.parseString(data, function (err, result) {
+    const xmlParser = xml2js.Parser()
+    xmlParser.parseString(xml, function (err, result) {
       if (err) {
         throw new AppException(`Error parsing xml: ${err}`)
       }
       that.json = result
     })
-    this.field = field
+    return this.json
   }
 
-  processChoice(choice) {
-    choice['xsd:']
-  }
-
-  processAnnotation(annotationJson) {
-    return annotationJson[0]['xsd:documentation'][0]
-  }
-
-  parseComplexType(complexType, resultObj) {
-    complexType.map((innerObj) => {
-      if (innerObj['xsd:attribute']) {
-        resultObj.attributes = this.processAttributes(innerObj['xsd:attribute'])
-      }
-      if (innerObj['xsd:sequence']) {
-        resultObj.elements = this.processElements(innerObj['xsd:sequence'])
-      }
-      if (innerObj['xsd:choice']) {
-        resultObj.choice = this.processChoice(innerObj['xsd:choice'])
-      }
-      if (innerObj['xsd:simpleContent']) {
-      }
-      if (innerObj['$']) {
-        forOwn(innerObj['$'], (value, key) => {
-          resultObj[key] = value
-        })
-      }
-    })
-    return resultObj
-  }
-
-  processElement(element) {
-    let returnObj = {}
-    forOwn(element['$'], (value, key) => {
-      returnObj = { ...returnObj, [key]: value }
-    })
-    return returnObj
-  }
-
-  parseType(type) {
-    return type.replace(/^xsd\:/, '')
-  }
-
-  processRestriction(restrictions) {
-    const enumerations = restrictions.map((restriction) => {
-      return restriction['$'].value
-    })
-    return enumerations
-  }
-
-  processAttributes(attributes) {
-    const result = attributes.map((attribute) => {
-      let ret = { name: attribute['$']['name'] }
-      if (attribute['$']) {
-        forOwn(attribute['$'], (value, key) => {
-          ret = { ...ret, [key]: this.parseType(value) }
-        })
-      }
-      if (attribute['xsd:simpleType']) {
-        ret = {
-          ...ret,
-          ['base']: this.parseType(
-            attribute['xsd:simpleType'][0]['xsd:restriction'][0]['$']['base']
-          ),
-          ['restriction']: this.processRestriction(
-            attribute['xsd:simpleType'][0]['xsd:restriction'][0]['xsd:enumeration']
-          ),
-        }
-      }
-      return ret
-    })
-    return result
-  }
-
-  processElements(sequence) {
-    if (isArray(sequence[0]['xsd:element'])) {
-      const elements = sequence[0]['xsd:element'].map((element) => {
-        return this.processElement(element)
-      })
-      return elements
-    } else {
-      return 'not array'
+  async extractZip(filePath) {
+    const dir = `${OPENIMMO_EXTRACT_FOLDER}/${this.generateRandomString(12)}`
+    try {
+      await extract(filePath, { dir })
+      this.dir = dir
+    } catch (err) {
+      // handle any errors
     }
   }
 
-  process() {
-    //console.log(this.json['xsd:schema']['xsd:element'][1])
-    //return this.json['xsd:schema']['xsd:element']
-    const result = this.json['xsd:schema']['xsd:element'].map((obj) => {
-      let resultObj = {}
-      if (obj['xsd:annotation']) {
-        resultObj.description = this.processAnnotation(obj['xsd:annotation'])
-      }
-      if (obj['xsd:complexType']) {
-        resultObj = this.parseComplexType(obj['xsd:complexType'], resultObj)
-      }
-      if (obj['$']) {
-        forOwn(obj['$'], (value, key) => {
-          if (key === 'type') {
-            resultObj[key] = this.parseType(value)
-          } else if (key !== 'name') {
-            resultObj[key] = value
-          }
-        })
-      }
-      return { [obj['$']['name']]: { ...resultObj } }
+  getDefinition(element) {
+    const definition = this.openImmoDefinition.find((elementDefinition) => {
+      return elementDefinition.name === element
     })
-    return result
+    return definition
+  }
+
+  validateElement(value, definition) {
+    if (definition.attributes) {
+      /* Attributes */
+      definition.attributes.map((attribute) => {
+        if (
+          attribute.name === 'wert' ||
+          attribute.name === 'erschl_attr' ||
+          attribute.name === 'stand' ||
+          definition.name === 'user_defined_simplefield'
+        ) {
+          //user_defined_extend problem
+        } else {
+          if (
+            has(attribute, 'use') &&
+            attribute.use === 'required' &&
+            !has(value[0]['$'], attribute.name)
+          ) {
+            throw new Error(`Validation Error: Attribute ${attribute.name} missing`)
+          }
+          if (
+            has(attribute, 'restriction') &&
+            !includes(attribute['restriction'], value[0]['$'][attribute.name])
+          ) {
+            throw new Error(
+              `Validation Error: Illegal value ${value[0]['$'][attribute.name]} for ${
+                attribute.name
+              }`
+            )
+          }
+        }
+      })
+    }
+
+    if (definition.sequence && isArray(definition.sequence)) {
+      /** sequence */
+      definition.sequence.map((sequence, index) => {
+        //FIXME sequence should have the same order as defined
+        if (!sequence.minOccurs && sequence.ref && !has(value[0], sequence.ref)) {
+          throw new Error(`Validation Error: Missing ${sequence.ref}`)
+        }
+        //recursion:
+        if (value[0][sequence.ref]) {
+          //we need to test because user_defined_anyfield will fail here
+          this.validateElement(value[0][sequence.ref], this.getDefinition(sequence.ref))
+        }
+      })
+    } else if (definition.simple) {
+      //this will have a value and attributes ie.
+      //<email_sonstige emailart="EM_DIREKT" bemerkung="1">foo@bar.de</email_sonstige>
+      //console.log('simple', definition, value)
+    } else if (definition.type) {
+      //console.log('type', definition.name, value, definition.type)
+      //FIXME: stellplatz type
+      if (definition.type === 'boolean' && !this.isBoolean(value[0])) {
+        throw new Error(`Validation Error: ${definition.name} must be boolean`)
+      } else if (definition.type === 'date' && !this.isDate(value[0])) {
+        throw new Error(`Validation Error: ${definition.name} must be a date`)
+      } else if (definition.type === 'string' && !this.isString(value[0])) {
+        throw new Error(`Validation Error: ${definition.name} must be a string`)
+      } else if (definition.type === 'decimal' && !this.isNumber(value[0])) {
+        throw new Error(`Validation Error: ${definition.name} must be a number`)
+      } else if (definition.type === 'positiveInteger' && !this.isPositiveInteger(value[0])) {
+        throw new Error(`Validation Error: ${definition.name} must be a positive integer`)
+      } else if (definition.type === 'dateTime' && !this.isDateTime(value[0])) {
+        throw new Error(`Validation Error: ${definition.name} must be a valid dateTime`)
+      } else if (definition.type === 'stellplatz' && !this.isParkingSpaceInfo(value[0])) {
+        throw new Error(`Validation Error: ${definition.name} must be a valid parking space info`)
+      }
+    }
+  }
+  isPositiveInteger(test) {
+    return test.match(/^[0-9]+$/)
+  }
+
+  isDateTime(test) {
+    //2001-01-01T11:00:00
+    return test.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}$/)
+  }
+  isDate(test) {
+    return test.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)
+  }
+
+  isNumber(test) {
+    return !isNaN(+test)
+  }
+
+  isParkingSpaceInfo(test) {
+    //FIXME: stellplatz must have: '$': { stellplatzmiete: '1', stellplatzkaufpreis: '1', anzahl: '1' }
+    return true
+  }
+
+  isString(test) {
+    return typeof test === 'string'
+  }
+
+  isBoolean(test) {
+    return includes(['0', '1', 'false', 'true'], test)
+  }
+
+  validateRoot(json) {
+    if (!json['openimmo']) {
+      throw new Error(`Invalid Openimmo xml.`)
+    }
+    const rootDefinition = this.getDefinition('openimmo')
+    rootDefinition.sequence.map((sequence) => {
+      if (!sequence.minOccurs && !json['openimmo'][sequence.ref]) {
+        throw new Error(`Validation Error: Missing ${sequence.ref}`)
+      }
+    })
+  }
+
+  validate(json) {
+    this.validateRoot(json)
+    const rootDefinition = this.getDefinition('openimmo')
+    const valid = rootDefinition.sequence.map((sequence) => {
+      this.validateElement(json['openimmo'][sequence.ref], this.getDefinition(sequence.ref))
+    })
+    return json
+  }
+
+  processProperties(json) {
+    const estates = json['openimmo']['anbieter'][0]['immobilie']
+    const map = require('../../resources/openimmo/openimmo-map.json')
+    const properties = estates.map((property) => {
+      let obj = {}
+      forOwn(map, (value, key) => {
+        obj = { ...obj, [key]: get(property, value) }
+      })
+      return obj
+    })
+    return properties
+  }
+
+  async process() {
+    let data
+    if (this.contentType === 'application/zip') {
+      await this.extractZip(this.filePath)
+      const files = fs.readdirSync(this.dir)
+      const file = files.find((file) => file.match(/\.xml$/))
+      data = fs.readFileSync(`${this.dir}/${file}`)
+    } else if (this.contentType === 'application/xml') {
+      data = fs.readFileSync(this.filePath)
+    }
+    let json = this.extractJson(data)
+    try {
+      if ((json = this.validate(json))) {
+        //return json
+        const properties = this.processProperties(json)
+        return properties
+        //return json['openimmo']['anbieter'][0]['immobilie'][0]
+      }
+    } catch (err) {
+      console.log(err.message)
+      return false
+    }
   }
 }
 
