33a64aa658e8d3a46b45c83ca8baaac91632aa2a
Justin Villocido <barudo@gmail.com>
2022-12-13 08:48:50 +0800

    migrations to alter some fields

---
+++ database/migrations/1670888191498_alter_fields_openimmo_standard_schema.js
@@ -0,0 +1,59 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+const Database = use('Database')
+const fields = [
+  'firing',
+  'energy_type',
+  'use_type',
+  'marketing_type',
+  'ground',
+  'parking_space_type',
+  'bath_options',
+]
+const has_defaults = [
+  'energy_type',
+  'use_type',
+  'marketing_type',
+  'parking_space_type', //6
+]
+const Promise = require('bluebird')
+const { includes } = require('lodash')
+
+class AlterFieldsOpenimmoStandardSchema extends Schema {
+  async up() {
+    Promise.map(fields, async (field) => {
+      if (includes(has_defaults, field)) {
+        await Database.raw(`ALTER TABLE estates
+        ALTER COLUMN ${field}
+        DROP DEFAULT`)
+      }
+
+      await Database.raw(`ALTER TABLE estates
+      ALTER COLUMN ${field} TYPE INTEGER[]
+      USING array[${field}]::INTEGER[]`)
+    })
+  }
+
+  async down() {
+    Promise.map(fields, async (field) => {
+      await Database.raw(
+        `ALTER TABLE estates ALTER COLUMN firing TYPE INTEGER USING firing[1]::INTEGER`
+      )
+      if (includes(has_defaults, field)) {
+        if (field === 'parking_space_type') {
+          await Database.raw(`ALTER TABLE estates
+            ALTER COLUMN ${field}
+            SET DEFAULT 6`)
+        } else {
+          await Database.raw(`ALTER TABLE estates
+            ALTER COLUMN ${field}
+            SET DEFAULT 1`)
+        }
+      }
+    })
+  }
+}
+
+module.exports = AlterFieldsOpenimmoStandardSchema
