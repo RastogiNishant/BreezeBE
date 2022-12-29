6cdcfcdfbea4b809d1ef5cb5c6a9aaad781838f9
Yong Yi Wong <yongyi.fullstack@gmail.com>
2022-12-29 07:56:06 +0000

    feedback fixed

--- database/migrations/1670334573039_adjust_notice_schema.js
+++ database/migrations/1670334573039_adjust_notice_schema.js
@@ -6,17 +6,12 @@ const Notice = use('App/Models/Notice')
 class AdjustNoticeSchema extends Schema {
   async up() {
     try {
-      const noticesCount = await Notice.query().whereNotNull('data').count()
-
       let notices = await Notice.query().whereNotNull('data').fetch()
 
       notices = notices.toJSON().filter((notice) => !notice.estate_id)
-      await Notice.query()
-        .where('id', notices[0].id)
-        .update({ estate_id: notices[0].data.estate_id })
       let i = 0
       while (i < notices.length) {
-        const partialNotices = notices.splice(i, 20)
+        const partialNotices = notices.slice(i, 20)
         await Promise.all(
           partialNotices.map(async (notice) => {
             await Notice.query().where('id', notice.id).update({ estate_id: notice.data.estate_id })
@@ -24,21 +19,6 @@ class AdjustNoticeSchema extends Schema {
         )
         i += 20
       }
-    } catch (e) {
-      console.log('AdjustNoticeSchema error=', e.message)
-    }
-  }
-
-  async adjust({ page, limit }) {
-    try {
-      let notices = await Notice.query().whereNotNull('data').fetch()
-
-      notices = notices.toJSON().filter((notice) => !notice.estate_id)
-      await Promise.all(
-        notices.map(async (notice) => {
-          await Notice.query().where('id', notice.id).update({ estate_id: notice.data.estate_id })
-        })
-      )
     } catch (e) {
       console.log('AdjustNoticeSchema error=', e.message)
     }
