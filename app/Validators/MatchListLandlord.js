84b13da32c4e54e205fcd9378a7b9380a8463532
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-01-10 14:12:20 +0000

    working on match filtering

--- app/Validators/MatchListLandlord.js
+++ app/Validators/MatchListLandlord.js
@@ -5,11 +5,42 @@ const { reduce } = require('lodash')
 
 const Base = require('./Base')
 const { id } = require('../Libs/schemas.js')
+const {
+  INCOME_TYPE_EMPLOYEE,
+  INCOME_TYPE_WORKER,
+  INCOME_TYPE_UNEMPLOYED,
+  INCOME_TYPE_CIVIL_SERVANT,
+  INCOME_TYPE_FREELANCER,
+  INCOME_TYPE_HOUSE_WORK,
+  INCOME_TYPE_PENSIONER,
+  INCOME_TYPE_SELF_EMPLOYED,
+  INCOME_TYPE_TRAINEE,
+} = require('../constants')
 
 class MatchListLandlord extends Base {
   static schema = () =>
     yup.object().shape({
       estate_id: id.required(),
+      budget_min: yup.number().positive().max(100),
+      budget_min: yup.number().positive().max(100),
+      budget_max: yup.number().positive().max(100),
+      credit_score_min: yup.number().positive().max(100),
+      credit_score_max: yup.number().positive().max(100),
+      income_type: yup
+        .array()
+        .of([
+          INCOME_TYPE_EMPLOYEE,
+          INCOME_TYPE_WORKER,
+          INCOME_TYPE_UNEMPLOYED,
+          INCOME_TYPE_CIVIL_SERVANT,
+          INCOME_TYPE_FREELANCER,
+          INCOME_TYPE_HOUSE_WORK,
+          INCOME_TYPE_PENSIONER,
+          INCOME_TYPE_SELF_EMPLOYED,
+          INCOME_TYPE_TRAINEE,
+        ]),
+      is_phone_verified: yup.boolean(),
+      is_id_verified: yup.boolean(),
       filters: yup.lazy((value) => {
         const itemsCount = reduce(value, (n, v, k) => (v ? n.concat(k) : n), []).length
         if (itemsCount > 1) {
