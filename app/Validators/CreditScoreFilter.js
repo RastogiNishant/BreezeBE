15cd1d983155e926ccb33c366105a567ef073d38
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-01-23 14:31:01 +0000

    min value changed

--- app/Validators/CreditScoreFilter.js
+++ app/Validators/CreditScoreFilter.js
@@ -4,8 +4,8 @@ const yup = require('yup')
 class CreditScoreFilter extends Base {
   static schema = () =>
     yup.object().shape({
-      credit_score_min: yup.number().positive().max(100),
-      credit_score_max: yup.number().positive().max(100),
+      credit_score_min: yup.number().min(0).max(100),
+      credit_score_max: yup.number().min(0).max(100),
     })
 }
 
