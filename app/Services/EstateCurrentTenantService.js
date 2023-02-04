144170f4efea50b7198dc9029372d5353a71fe19
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-02-04 04:25:32 +0000

    Bug fixed

--- app/Services/EstateCurrentTenantService.js
+++ app/Services/EstateCurrentTenantService.js
@@ -449,7 +449,7 @@ class EstateCurrentTenantService extends BaseService {
             successCount: inviteResult.successCount,
             failureCount: inviteResult.failureCount,
             email: {
-              sucessCount: inviteResult.successCount,
+              successCount: inviteResult.successCount,
               failureCount: inviteResult.failureCount,
             },
           }
@@ -459,7 +459,7 @@ class EstateCurrentTenantService extends BaseService {
             successCount: inviteResult.successCount,
             failureCount: inviteResult.failureCount,
             phone: {
-              sucessCount: inviteResult.successCount,
+              successCount: inviteResult.successCount,
               failureCount: inviteResult.failureCount,
             },
           }
@@ -487,7 +487,6 @@ class EstateCurrentTenantService extends BaseService {
         phone,
         surname,
       })
-
       if (singleResult?.successCount) {
         result.successCount = (result?.successCount || 0) + singleResult.successCount
       }
@@ -496,6 +495,7 @@ class EstateCurrentTenantService extends BaseService {
       }
 
       if (singleResult?.email) {
+        console.log('inviteTenantToApp=', singleResult?.email)
         if (singleResult?.email?.successCount) {
           result.email.successCount =
             (result?.email?.successCount || 0) + singleResult.email.successCount
