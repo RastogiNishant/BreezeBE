dca7caaa811e6b2267d5a98a4c99e321d10f3dbd
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-02-16 09:12:19 +0000

    added contraint for room type

--- app/Validators/CreateRoom.js
+++ app/Validators/CreateRoom.js
@@ -3,17 +3,90 @@
 const yup = require('yup')
 const Base = require('./Base')
 const { id } = require('../Libs/schemas.js')
+const {
+  ROOM_TYPE_GUEST_ROOM,
+  ROOM_TYPE_BATH,
+  ROOM_TYPE_BEDROOM,
+  ROOM_TYPE_KITCHEN,
+  ROOM_TYPE_CORRIDOR,
+  ROOM_TYPE_OFFICE,
+  ROOM_TYPE_PANTRY,
+  ROOM_TYPE_CHILDRENS_ROOM,
+  ROOM_TYPE_BALCONY,
+  ROOM_TYPE_WC,
+  ROOM_TYPE_OTHER_SPACE,
+  ROOM_TYPE_CHECKROOM,
+  ROOM_TYPE_DINING_ROOM,
+  ROOM_TYPE_ENTRANCE_HALL,
+  ROOM_TYPE_GYM,
+  ROOM_TYPE_IRONING_ROOM,
+  ROOM_TYPE_LIVING_ROOM,
+  ROOM_TYPE_LOBBY,
+  ROOM_TYPE_MASSAGE_ROOM,
+  ROOM_TYPE_STORAGE_ROOM,
+  ROOM_TYPE_PLACE_FOR_GAMES,
+  ROOM_TYPE_SAUNA,
+  ROOM_TYPE_SHOWER,
+  ROOM_TYPE_STAFF_ROOM,
+  ROOM_TYPE_SWIMMING_POOL,
+  ROOM_TYPE_TECHNICAL_ROOM,
+  ROOM_TYPE_TERRACE,
+  ROOM_TYPE_WASHING_ROOM,
+  ROOM_TYPE_EXTERNAL_CORRIDOR,
+  ROOM_TYPE_STAIRS,
+  ROOM_TYPE_PROPERTY_ENTRANCE,
+  ROOM_TYPE_GARDEN,
+  ROOM_TYPE_LOGGIA,
+} = require('../constants')
 
 class CreateRoom extends Base {
   static schema = () =>
     yup.object().shape({
       estate_id: id.required(),
       name: yup.string().max(255),
-      type: yup.number().positive().required(),
+      type: yup
+        .number()
+        .positive()
+        .oneof([
+          ROOM_TYPE_GUEST_ROOM,
+          ROOM_TYPE_BATH,
+          ROOM_TYPE_BEDROOM,
+          ROOM_TYPE_KITCHEN,
+          ROOM_TYPE_CORRIDOR,
+          ROOM_TYPE_OFFICE,
+          ROOM_TYPE_PANTRY,
+          ROOM_TYPE_CHILDRENS_ROOM,
+          ROOM_TYPE_BALCONY,
+          ROOM_TYPE_WC,
+          ROOM_TYPE_OTHER_SPACE,
+          ROOM_TYPE_CHECKROOM,
+          ROOM_TYPE_DINING_ROOM,
+          ROOM_TYPE_ENTRANCE_HALL,
+          ROOM_TYPE_GYM,
+          ROOM_TYPE_IRONING_ROOM,
+          ROOM_TYPE_LIVING_ROOM,
+          ROOM_TYPE_LOBBY,
+          ROOM_TYPE_MASSAGE_ROOM,
+          ROOM_TYPE_STORAGE_ROOM,
+          ROOM_TYPE_PLACE_FOR_GAMES,
+          ROOM_TYPE_SAUNA,
+          ROOM_TYPE_SHOWER,
+          ROOM_TYPE_STAFF_ROOM,
+          ROOM_TYPE_SWIMMING_POOL,
+          ROOM_TYPE_TECHNICAL_ROOM,
+          ROOM_TYPE_TERRACE,
+          ROOM_TYPE_WASHING_ROOM,
+          ROOM_TYPE_EXTERNAL_CORRIDOR,
+          ROOM_TYPE_STAIRS,
+          ROOM_TYPE_PROPERTY_ENTRANCE,
+          ROOM_TYPE_GARDEN,
+          ROOM_TYPE_LOGGIA,
+        ])
+        .required(),
       area: yup.number().min(0),
       options: yup.array().of(yup.string().lowercase().trim()),
       favorite: yup.boolean(),
-      order: yup.number().positive()
+      order: yup.number().positive(),
     })
 }
 
