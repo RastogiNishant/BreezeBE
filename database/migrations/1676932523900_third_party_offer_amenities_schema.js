b4ddcf5abeb2784eb9fdd561782dcca6b7c2c6c4
Justin Villocido <barudo@gmail.com>
2023-02-21 15:44:45 +0800

    index status and source_id

--- database/migrations/1676932523900_third_party_offer_amenities_schema.js
+++ database/migrations/1676932523900_third_party_offer_amenities_schema.js
@@ -6,12 +6,15 @@ const Schema = use('Schema')
 class ThirdPartyOfferAmenitiesSchema extends Schema {
   up() {
     this.table('third_party_offers', (table) => {
+      table.integer('source_id').alter().index()
+      table.smallint('status').index()
       table.specificType('amenities', 'text array')
     })
   }
 
   down() {
     this.table('third_party_offers', (table) => {
+      table.dropColumn('status')
       table.dropColumn('amenities')
     })
   }
