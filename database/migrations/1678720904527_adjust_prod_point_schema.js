34166c9bae6f3e4058a68fb4a3230e02b775a7ff
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-03-13 16:02:40 +0000

    Fixed  getting zone of points for GeoApify

---
+++ database/migrations/1678720904527_adjust_prod_point_schema.js
@@ -0,0 +1,46 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+const Point = use('App/Models/Point')
+const GeoService = use('App/Services/GeoService')
+
+class AdjustProdPointSchema extends Schema {
+  async up() {
+    const points = (
+      await Point.query()
+
+        .whereIn(
+          'id',
+          // [
+          //   3711, 651, 2618, 700, 2533, 942, 1169, 1821, 1824, 1652, 1897, 2024, 2039, 2076, 2920,
+          //   2614, 2699, 2774, 3146, 3792,
+          // ]
+          [3711]
+        )
+        .fetch()
+    ).toJSON()
+    console.log('AdjustProdPointSchema=', points[0])
+    let i = 0
+    while (i < points.length) {
+      const point = points[i]
+      const polygon = await GeoApify.createIsoline(
+        { lat: point.lat, lon: point.lon },
+        point.distType,
+        point.dist_min
+      )
+      const zone = GeoService.getPointsQuery(polygon)
+      point.zone = zone
+      await point.save()
+      i++
+    }
+  }
+
+  down() {
+    this.table('adjust_prod_points', (table) => {
+      // reverse alternations
+    })
+  }
+}
+
+module.exports = AdjustProdPointSchema
