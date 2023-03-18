002fb88adaa9a268ea80302360e37e62105c2a72
Justin Villocido <barudo@gmail.com>
2023-03-18 09:46:22 +0800

    add more fields to validate

--- app/Validators/CreateThirdPartyOffer.js
+++ app/Validators/CreateThirdPartyOffer.js
@@ -2,14 +2,16 @@
 
 const yup = require('yup')
 const Base = require('./Base')
-const { THIRD_PARTY_OFFER_SOURCES } = require('../constants')
+const { THIRD_PARTY_OFFER_SOURCES, STATUS_ACTIVE, STATUS_EXPIRE } = require('../constants')
+const moment = require('moment')
+const currentYear = moment().format('Y')
 
 class CreateThirdPartyOffer extends Base {
   static schema = () =>
     yup.object().shape({
       source: yup.string().oneOf(THIRD_PARTY_OFFER_SOURCES).required(),
       source_id: yup.number().positive().required(),
-      coord: yup.string().matches(),
+      coord: yup.string().matches(/[0-9\.]+,[0-9\.]+/),
       description: yup.string(),
       url: yup
         .string()
@@ -20,6 +22,25 @@ class CreateThirdPartyOffer extends Base {
       street: yup.string().max(100),
       city: yup.string().max(100),
       zip: yup.string().max(10),
+      country: yup.string().max(50),
+      address: yup.string().max(255),
+      floor: yup.number().integer(),
+      floor_count: yup.number().integer(),
+      bathrooms: yup.number().integer(),
+      rooms: yup.number(),
+      area: yup.number().integer(),
+      construction_year: yup.number().min(1000).max(+currentYear),
+      images: yup.object(),
+      energy_efficiency_class: yup.string().matches(/^[A-F]{1}[\-\+]?$/),
+      vacant_from: yup.date().min(moment().format('Y-m-d')),
+      visit_from: yup.date().min(moment().format('Y-m-d')),
+      visit_to: yup.date().min(moment().format('Y-m-d')),
+      status: yup.number().oneOf([STATUS_ACTIVE, STATUS_EXPIRE]),
+      amenities: yup.array().of(yup.string()),
+      coord_raw: yup.string().matches(/[0-9\.]+,[0-9\.]+/),
+      expiration_date: yup.date().min(moment().format('Y-m-d')),
+      price: yup.number(),
+      contact: yup.object(),
     })
 }
 
