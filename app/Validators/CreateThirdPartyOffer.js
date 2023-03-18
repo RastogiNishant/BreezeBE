ab8c70b984715db22f81516ef020d6fd99615c00
Justin Villocido <barudo@gmail.com>
2023-03-18 13:21:38 +0800

    make energy effeciency to A-H. Add nullable to some

--- app/Validators/CreateThirdPartyOffer.js
+++ app/Validators/CreateThirdPartyOffer.js
@@ -2,7 +2,12 @@
 
 const yup = require('yup')
 const Base = require('./Base')
-const { THIRD_PARTY_OFFER_SOURCES, STATUS_ACTIVE, STATUS_EXPIRE } = require('../constants')
+const {
+  THIRD_PARTY_OFFER_SOURCES,
+  STATUS_ACTIVE,
+  STATUS_EXPIRE,
+  VALID_URL_REG_EXP,
+} = require('../constants')
 const moment = require('moment')
 const currentYear = moment().format('Y')
 
@@ -13,32 +18,31 @@ class CreateThirdPartyOffer extends Base {
       source_id: yup.number().positive().required(),
       coord: yup.string().matches(/[0-9\.]+,[0-9\.]+/),
       description: yup.string(),
-      url: yup
-        .string()
-        .matches(
-          /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
-        ),
+      url: yup.string().matches(VALID_URL_REG_EXP),
       house_number: yup.string().max(10),
       street: yup.string().max(100),
       city: yup.string().max(100),
       zip: yup.string().max(10),
       country: yup.string().max(50),
       address: yup.string().max(255),
-      floor: yup.number().integer(),
-      floor_count: yup.number().integer(),
-      bathrooms: yup.number().integer(),
-      rooms: yup.number(),
-      area: yup.number().integer(),
-      construction_year: yup.number().min(1000).max(+currentYear),
-      images: yup.object(),
-      energy_efficiency_class: yup.string().matches(/^[A-F]{1}[\-\+]?$/),
-      vacant_from: yup.date().min(moment().format('Y-m-d')),
-      visit_from: yup.date().min(moment().format('Y-m-d')),
-      visit_to: yup.date().min(moment().format('Y-m-d')),
+      floor: yup.number().integer().nullable(),
+      floor_count: yup.number().integer().nullable(),
+      bathrooms: yup.number().integer().nullable(),
+      rooms: yup.number().nullable(),
+      area: yup.number().integer().nullable(),
+      construction_year: yup.number().min(1000).max(+currentYear).nullable(),
+      images: yup.string(),
+      energy_efficiency_class: yup
+        .string()
+        .matches(/^[A-H]{1}[\-\+]*$/)
+        .nullable(),
+      vacant_from: yup.string().matches(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/),
+      visit_from: yup.string().matches(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/),
+      visit_to: yup.string().matches(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/),
       status: yup.number().oneOf([STATUS_ACTIVE, STATUS_EXPIRE]),
       amenities: yup.array().of(yup.string()),
       coord_raw: yup.string().matches(/[0-9\.]+,[0-9\.]+/),
-      expiration_date: yup.date().min(moment().format('Y-m-d')),
+      expiration_date: yup.string().matches(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/),
       price: yup.number(),
       contact: yup.object(),
     })
