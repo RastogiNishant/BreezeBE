fb274e55f749c9569c4ec425c1e156f461bbaaee
Justin Villocido <barudo@gmail.com>
2023-03-27 20:06:51 +0800

    likes and dislikes should not show offers that were knocked

--- app/Services/ThirdPartyOfferService.js
+++ app/Services/ThirdPartyOfferService.js
@@ -303,12 +303,12 @@ class ThirdPartyOfferService {
     if (like) {
       field = 'liked'
       value = true
-      query.select(Database.raw(`tpoi.updated_at as action_at`))
+      query.select(Database.raw(`tpoi.updated_at as action_at`)).whereNot('knocked', true)
     } else if (dislike) {
       field = 'liked'
       value = false
       query.orWhere('third_party_offers.status', STATUS_EXPIRE)
-      query.select(Database.raw(`tpoi.updated_at as action_at`))
+      query.select(Database.raw(`tpoi.updated_at as action_at`)).whereNot('knocked', true)
       //if dislike, include both active and expired
     } else if (knock) {
       field = 'knocked'
