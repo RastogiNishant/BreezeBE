0438c86e7c7e2b6fd8440b0d0e213c9a104c9bb3
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-04-05 14:11:26 +0000

    working on outside landlord invitation

---
+++ app/Services/AppService.js
@@ -0,0 +1,10 @@
+'use strict'
+const { FirebaseDynamicLinks } = use('firebase-dynamic-links')
+
+class AppService {
+  static async createTenantDynamicLink() {
+
+  }
+}
+
+module.exports = AppService
