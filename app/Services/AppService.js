64080a14dd0fd92184dc181a0e363b34ff61008a
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-04-05 16:11:10 +0000

    completed outside tenant invitation by tenant

--- app/Services/AppService.js
+++ app/Services/AppService.js
@@ -4,7 +4,6 @@ const { createDynamicLink } = require('../Libs/utils')
 class AppService {
   static async createTenantDynamicLink() {
     const link = `${process.env.DEEP_LINK}?type=new_tenant`
-    console.log('link here=', link)
     return await createDynamicLink(link)
   }
 }
