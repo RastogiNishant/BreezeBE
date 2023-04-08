da16e7656b1d18f63d41c8e1ad1890e622ea6af0
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-04-08 10:59:06 +0000

    improved code quality

--- app/Classes/OhneMakler.js
+++ app/Classes/OhneMakler.js
@@ -8,6 +8,7 @@ const {
   PROPERTY_TYPE_HOUSE,
   PROPERTY_TYPE_SHORT_TERM,
   DATE_FORMAT,
+  STATUS_EXPIRE,
 } = require('../constants')
 const { isEmpty } = require('lodash')
 const moment = require('moment')
@@ -64,49 +65,68 @@ class OhneMakler {
 
   mapEstate(estate) {
     let newEstate
-    for (const [key, value] of Object.entries(this.map)) {
-      newEstate = { ...newEstate, [value]: estate[key] }
-    }
-    newEstate.source = THIRD_PARTY_OFFER_SOURCE_OHNE_MAKLER
-    newEstate.address = `${estate.address}, ${estate.postcode} ${estate.city}, ${estate.country}`
+    try {
+      for (const [key, value] of Object.entries(this.map)) {
+        newEstate = { ...newEstate, [value]: estate[key] }
+      }
+      newEstate.source = THIRD_PARTY_OFFER_SOURCE_OHNE_MAKLER
+      newEstate.address = `${estate.address}, ${estate.postcode} ${estate.city}, ${estate.country}`
 
-    if (estate?.pictures && !Array.isArray(estate?.pictures)) {
-      estate.pictures = [estate?.pictures]
-    }
-    newEstate.images = estate?.pictures?.length ? JSON.stringify(estate.pictures) : null
+      if (estate?.pictures && !Array.isArray(estate?.pictures)) {
+        estate.pictures = [estate?.pictures]
+      }
+      newEstate.images = estate?.pictures?.length ? JSON.stringify(estate.pictures) : null
 
-    if (!isEmpty(estate.ausstattung)) {
-      newEstate.amenities = estate.ausstattung.split(', ')
-    }
-    newEstate.status = STATUS_ACTIVE
-    newEstate.coord = `${estate.latitude},${estate.longitude}`
-    newEstate.coord_raw = `${estate.latitude},${estate.longitude}`
-    if (estate.uebernahme_ab && estate.uebernahme_ab.match(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/)) {
-      newEstate.vacant_date = estate.uebernahme_ab
-    } else {
-      newEstate.vacant_date = moment.utc(new Date()).format(DATE_FORMAT)
-      newEstate.vacant_from_string = estate.uebernahme_ab
-    }
+      if (!isEmpty(estate.ausstattung)) {
+        newEstate.amenities = estate.ausstattung.split(', ')
+      }
+      newEstate.status = STATUS_ACTIVE
+      newEstate.coord = `${estate.latitude},${estate.longitude}`
+      newEstate.coord_raw = `${estate.latitude},${estate.longitude}`
+      if (estate.uebernahme_ab && estate.uebernahme_ab.match(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/)) {
+        newEstate.vacant_date = moment
+          .utc(estate.uebernahme_ab, 'YYYY/MM/DD', true)
+          .format(DATE_FORMAT)
+      } else {
+        newEstate.vacant_date = moment.utc(new Date()).format(DATE_FORMAT)
+        newEstate.vacant_from_string = estate.uebernahme_ab
+      }
 
-    //as confirmed by andrey, K is Kellergeschoss (basement or underground)
-    if (newEstate.floor === 'K') {
-      newEstate.floor = -1
-    }
-    //11 here means greater than 5
-    if (newEstate.floor === '>') {
-      newEstate.floor = 11
-    }
-    newEstate.energy_efficiency_class = estate?.energieausweis?.energieeffizienzklasse
-    newEstate.property_type = this.parsePropertyType(estate.objektart)
+      //as confirmed by andrey, K is Kellergeschoss (basement or underground)
+      if (newEstate.floor === 'K') {
+        newEstate.floor = -1
+      }
+      //11 here means greater than 5
+      if (newEstate.floor === '>') {
+        newEstate.floor = 11
+      }
+      newEstate.energy_efficiency_class = estate?.energieausweis?.energieeffizienzklasse
+      newEstate.property_type = this.parsePropertyType(estate.objektart)
 
-    if (!newEstate.extra_costs && newEstate.additional_costs && newEstate.heating_costs) {
-      //extra costs is the sum of additional_costs and heating_costs
-      newEstate.extra_costs = +newEstate.additional_costs + +newEstate.heating_costs
+      if (!newEstate.extra_costs && newEstate.additional_costs && newEstate.heating_costs) {
+        //extra costs is the sum of additional_costs and heating_costs
+        newEstate.extra_costs = +newEstate.additional_costs + +newEstate.heating_costs
+      }
+
+      if (estate?.expiration_date) {
+        newEstate.available_end_at = moment
+          .utc(estate.expiration_date, 'YYYY/MM/DD', true)
+          .add(1, 'day')
+          .format(DATE_FORMAT)
+      }
+
+      if (
+        !newEstate?.available_end_at ||
+        newEstate.available_end_at > moment.utc(new Date()).format(DATE_FORMAT)
+      ) {
+        newEstate.status = STATUS_ACTIVE
+      } else {
+        console.log('expire estate=', estate.id)
+        newEstate.status = STATUS_EXPIRE
+      }
+    } catch (e) {
+      console.log('e.estate', estate)
     }
-    newEstate.available_end_at = moment
-      .utc(estate.expiration_date)
-      .add(1, 'day')
-      .format(DATE_FORMAT)
     return newEstate
   }
 
@@ -121,15 +141,23 @@ class OhneMakler {
     if (!estates) {
       estates = this.data
     }
-    const processableEstates = estates.reduce((current, estate) => {
-      if (this.estateCanBeProcessed(estate)) {
-        estate = this.mapEstate(estate)
-        return [...current, estate]
-      } else {
-        return current
-      }
-    }, [])
-    return processableEstates
+    try {
+      const processableEstates = estates.reduce((current, estate) => {
+        if (this.estateCanBeProcessed(estate)) {
+          estate = this.mapEstate(estate)
+          if (estate) {
+            return [...current, estate]
+          }
+          return current
+        } else {
+          return current
+        }
+      }, [])
+      return processableEstates
+    } catch (e) {
+      console.log('OhneMakler error', e.message)
+      return null
+    }
   }
 }
 
