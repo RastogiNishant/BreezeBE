fe7b784804cf0915f65f8e94afd09b12ed3348f9
Justin Villocido <barudo@gmail.com>
2023-04-11 12:07:29 +0800

    add test to parseHouseAndApartmentTypes

--- test/unit/class-ohnemakler.spec.js
+++ test/unit/class-ohnemakler.spec.js
@@ -1,5 +1,12 @@
 'use strict'
 
+const {
+  HOUSE_TYPE_HIGH_RISE,
+  APARTMENT_TYPE_ATTIC,
+  HOUSE_TYPE_COUNTRY,
+  PROPERTY_TYPE_APARTMENT,
+} = require('../../app/constants')
+
 const { test } = use('Test/Suite')('Class Ohnemakler')
 const OhneMakler = use('App/Classes/OhneMakler')
 
@@ -38,4 +45,31 @@ test('parseItemType will return null if type does not exist', async ({ assert })
 test('parseItemType will return null if value does not exist', async ({ assert }) => {
   const ohneMakler = new OhneMakler()
   assert.equal(ohneMakler.parseItemType('none-existing-value', 'propertyType'), null)
+})
+
+test('parseHouseAndApartmentTypes returns expected newEstate', async ({ assert }) => {
+  const ohneMakler = new OhneMakler()
+  const testEstates = [
+    { property_type: 'Mehrfamilienhaus', objekttyp: 'Mehrfamilienhaus' },
+    { property_type: 'Mehrfamilienhaus', objekttyp: 'Dachgeschosswohnung' },
+    { property_type: 'Dachgeschosswohnung', objekttyp: 'Stadthaus' },
+    { property_type: 'Non-existing type' },
+    { objekttyp: 'Non-existing type' },
+    { property_type: 'Non-existing type', objekttyp: 'Non-existing type' },
+    { property_type: 'Etagenwohnung' },
+  ]
+
+  const expectedEstates = [
+    { house_type: HOUSE_TYPE_HIGH_RISE },
+    { house_type: HOUSE_TYPE_HIGH_RISE, apt_type: APARTMENT_TYPE_ATTIC },
+    { house_type: HOUSE_TYPE_COUNTRY, apt_type: APARTMENT_TYPE_ATTIC },
+    {},
+    {},
+    {},
+    { property_type: PROPERTY_TYPE_APARTMENT },
+  ]
+  for (let i = 0; i < testEstates.length; i++) {
+    console.log(ohneMakler.parseHouseAndApartmentTypes(testEstates[i], {}), expectedEstates[i])
+    assert.deepEqual(ohneMakler.parseHouseAndApartmentTypes(testEstates[i], {}), expectedEstates[i])
+  }
 })
