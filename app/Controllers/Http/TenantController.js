8f0275c3786d5ec3e8f9c0e31db31e744be85870
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-04-14 07:23:07 +0000

    added is_short_term_rent

--- app/Controllers/Http/TenantController.js
+++ app/Controllers/Http/TenantController.js
@@ -104,6 +104,11 @@ class TenantController {
         throw new HttpException('Transfer budget min has to be smaller than max', 400)
       }
 
+      if (data.is_short_term_rent) {
+        data.residency_duration_min = null
+        data.residency_duration_max = null
+      }
+
       await tenant.updateItemWithTrx(data, trx)
       const { lat, lon } = tenant.getLatLon()
 
