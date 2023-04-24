5c5dba9cd302ee5f3e47bcbe5c89d49883df0370
Justin Villocido <barudo@gmail.com>
2023-04-24 21:06:52 +0800

    update publish Estate validator

--- app/Validators/PublishEstate.js
+++ app/Validators/PublishEstate.js
@@ -2,11 +2,18 @@
 
 const yup = require('yup')
 const Base = require('./Base')
+const { THIRD_PARTY_PUBLISHERS } = require('../constants')
 
 class PublishEstate extends Base {
   static schema = () =>
     yup.object().shape({
       action: yup.string().oneOf(['publish', 'unpublish']).required(),
+      from_web: yup.string().oneOf(['1']),
+      publishers: yup.array().when('from_web', {
+        is: '1',
+        then: yup.array().of(yup.string().oneOf(THIRD_PARTY_PUBLISHERS)),
+      }),
+      confirm_incomplete: yup.boolean().default(false),
     })
 }
 
