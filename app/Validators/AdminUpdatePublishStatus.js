0b68cf51261482080c53727c2b6cca28d3bdeaa4
Justin Villocido <barudo@gmail.com>
2023-05-16 19:18:51 +0800

    update validator for admin publish

--- app/Validators/AdminUpdatePublishStatus.js
+++ app/Validators/AdminUpdatePublishStatus.js
@@ -1,11 +1,13 @@
 'use strict'
 const yup = require('yup')
 const Base = require('./Base')
+const { THIRD_PARTY_PUBLISHERS } = require('../constants')
 
 class AdminUpdatePublishStatus extends Base {
   static schema = () =>
     yup.object().shape({
-      action: yup.string().oneOf(['unpublish']),
+      action: yup.string().oneOf(['unpublish', 'publish', 'publish-marketplace']),
+      publishers: yup.array().of(yup.string().oneOf(THIRD_PARTY_PUBLISHERS)),
       ids: yup
         .array()
         .of(yup.number().integer().positive())
