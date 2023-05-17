5110186e42f005fc863c783729e2dcc8bdfc3305
Justin Villocido <barudo@gmail.com>
2023-05-17 08:52:01 +0800

    update admin publish validator

--- app/Validators/AdminUpdatePublishStatus.js
+++ app/Validators/AdminUpdatePublishStatus.js
@@ -8,11 +8,21 @@ class AdminUpdatePublishStatus extends Base {
     yup.object().shape({
       action: yup.string().oneOf(['unpublish', 'publish', 'publish-marketplace']),
       publishers: yup.array().of(yup.string().oneOf(THIRD_PARTY_PUBLISHERS)),
-      ids: yup
-        .array()
-        .of(yup.number().integer().positive())
-        .required('missing field: ids')
-        .typeError('ids must be an array of integers'),
+      ids: yup.array().when('action', {
+        is: 'unpublish',
+        then: yup
+          .array()
+          .of(yup.number().integer().positive())
+          .required('missing field: ids')
+          .typeError('ids must be an array of integers'),
+      }),
+      id: yup
+        .number()
+        .integer()
+        .when('action', {
+          is: (action) => action === 'publish' || action === 'publish-marketplace',
+          then: yup.number().integer().required('id is required'),
+        }),
     })
 }
 
