d9bcb19aafd2a48e17e0dfb5a5d076922101005b
Justin Villocido <barudo@gmail.com>
2023-05-26 20:47:27 +0800

    consider all cases

--- database/migrations/1685097380384_migrate_apartment_type_third_party_offers_schema.js
+++ database/migrations/1685097380384_migrate_apartment_type_third_party_offers_schema.js
@@ -3,12 +3,33 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 const Database = use('Database')
-const { APARTMENT_TYPE_ATTIC } = require('../../app/constants')
+const {
+  APARTMENT_TYPE_ATTIC,
+  APARTMENT_TYPE_FLAT,
+  APARTMENT_TYPE_GALLERY,
+  APARTMENT_TYPE_HOLIDAY,
+} = require('../../app/constants')
 
 class MigrateApartmentTypeThirdPartyOffersSchema extends Schema {
   async up() {
+    //14 used to be APARTMENT_TYPE_ATTIC
+    //13 used to be APARTMENT_TYPE_RAW_ATTIC
+    //12 used to be APARTMENT_TYPE_GALLERY
+    //11 used to be APARTMENT_TYPE_HOLIDAY
+    //10 used to be APARTMENT_TYPE_ETAGE
     await Database.raw(`
-      update third_party_offers tpo set apt_type=${APARTMENT_TYPE_ATTIC} where tpo.apt_type=14
+      update third_party_offers tpo set apt_type=
+        CASE 
+          WHEN tpo1.apt_type = 14 THEN ${APARTMENT_TYPE_ATTIC}
+          WHEN tpo1.apt_type = 13 THEN ${APARTMENT_TYPE_ATTIC}
+          WHEN tpo1.apt_type = 12 THEN ${APARTMENT_TYPE_GALLERY}
+          WHEN tpo1.apt_type = 11 THEN ${APARTMENT_TYPE_HOLIDAY}
+          WHEN tpo1.apt_type = 10 THEN ${APARTMENT_TYPE_FLAT}
+          ELSE tpo1.apt_type
+        END  
+      FROM
+        third_party_offers tpo1
+      WHERE tpo1.id=tpo.id
     `)
   }
 
