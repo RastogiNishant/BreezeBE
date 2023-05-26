73111cfa2b28e47db07a149e9b7be94ccc24a4fb
Justin Villocido <barudo@gmail.com>
2023-05-26 20:20:02 +0800

    use constant

--- database/migrations/1685097380384_migrate_apartment_type_third_party_offers_schema.js
+++ database/migrations/1685097380384_migrate_apartment_type_third_party_offers_schema.js
@@ -3,11 +3,12 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 const Database = use('Database')
+const { APARTMENT_TYPE_ATTIC } = require('../../app/constants')
 
 class MigrateApartmentTypeThirdPartyOffersSchema extends Schema {
   async up() {
     await Database.raw(`
-      update third_party_offers tpo set apt_type=12 where tpo.apt_type=14
+      update third_party_offers tpo set apt_type=${APARTMENT_TYPE_ATTIC} where tpo.apt_type=14
     `)
   }
 
