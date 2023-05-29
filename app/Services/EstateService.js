ac0cbcd5c2cb1a249afc9a066b84a8562b1f8dac
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-05-29 07:10:44 -0500

    removed unnecessary amenity

--- app/Services/EstateService.js
+++ app/Services/EstateService.js
@@ -396,7 +396,6 @@ class EstateService {
       .withCount('inviteBuddies')
       .with('point')
       .with('files')
-      .with('amenities')
       .with('current_tenant', function (q) {
         q.with('user')
       })
