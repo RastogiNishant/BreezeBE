aba0eaeda9be7fd80e125f5c4b90058f6dafbc04
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-06-29 06:06:11 -0500

    fixed updated tenant

--- app/Controllers/Http/TenantController.js
+++ app/Controllers/Http/TenantController.js
@@ -110,19 +110,18 @@ class TenantController {
         data.residency_duration_max = null
       }
 
-      await tenant.updateItemWithTrx(data, trx)
-      const { lat, lon } = tenant.getLatLon()
-
       // Deactivate tenant on personal data change
       const shouldDeactivateTenant = without(Object.keys(data), ...Tenant.updateIgnoreFields).length
+
       if (shouldDeactivateTenant) {
-        updatedTenant.notify_sent = [NOTICE_TYPE_TENANT_PROFILE_FILL_UP_ID]
-        updatedTenant.status = STATUS_DRAFT
+        tenant.notify_sent = [NOTICE_TYPE_TENANT_PROFILE_FILL_UP_ID]
+        tenant.status = STATUS_DRAFT
       } else {
       }
-      await trx.commit()
 
-      const updatedTenant = await Tenant.find(tenant.id)
+      await tenant.updateItemWithTrx(data, trx)
+
+      await trx.commit()
 
       // Add tenant anchor zone processing
       if (lat !== undefined && lat !== null && lon !== undefined && lon !== null) {
