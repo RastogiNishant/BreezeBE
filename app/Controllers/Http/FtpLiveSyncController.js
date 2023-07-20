a355351dc0b3464bb05e4a598ce5fefecf5a007e
Justin Villocido <barudo@gmail.com>
2023-07-20 19:44:08 +0800

    add update delete to controller

--- app/Controllers/Http/FtpLiveSyncController.js
+++ app/Controllers/Http/FtpLiveSyncController.js
@@ -1,16 +1,31 @@
-'use_strict'
+'use strict'
+
+const FtpLiveSyncService = use('App/Services/FtpLiveSyncService')
 
 class FtpLiveSyncController {
   async get({ auth, response }) {
-    response.res(true)
+    const ftpAccount = await FtpLiveSyncService.get(auth.user.id)
+    response.res(ftpAccount)
   }
 
   async add({ request, auth, response }) {
+    const { company, email } = request.all()
+    const user_id = auth.user.id
+    await FtpLiveSyncService.create({ company, email, user_id })
     response.res(true)
   }
 
   async delete({ auth, response }) {
+    const user_id = auth.user.id
+    await FtpLiveSyncService.delete(user_id)
     response.res(true)
+  }
+
+  async update({ request, auth, response }) {
+    const { company, email } = request.all()
+    const user_id = auth.user.id
+    const ftpAccount = await FtpLiveSyncService.update(user_id, { company, email })
+    response.res(ftpAccount)
   }
 }
 
