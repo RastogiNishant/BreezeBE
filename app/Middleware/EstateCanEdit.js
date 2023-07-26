eda47ae13c243e72ba4f0428e332d4bf7c3cdc9e
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-07-26 10:25:35 -0500

    working on disabling estates for published property

---
+++ app/Middleware/EstateCanEdit.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const HttpException = require('../Exceptions/HttpException')
+const { ROLE_LANDLORD, PUBLISH_STATUS_APPROVED_BY_ADMIN } = require('../constants')
+const EstateService = use('App/Services/EstateService')
+const {
+  exceptions: {
+    ERROR_WRONG_ROLE,
+    ERROR_NO_AUTHENTICATE,
+    WRONG_PARAMS,
+    NO_ESTATE_EXIST,
+    ERROR_PROPERTY_PUBLISHED_CAN_BE_EDITABLE,
+  },
+} = require('../exceptions')
+class EstateCanEdit {
+  async handle({ auth, request }, next) {
+    // Skip anonymous routes and admin routes check
+    if (!auth?.user) {
+      throw new HttpException(ERROR_NO_AUTHENTICATE, 400)
+    }
+
+    const { role } = auth.user
+    if (role !== ROLE_LANDLORD) {
+      throw new HttpException(ERROR_WRONG_ROLE, 400)
+    }
+
+    const { id, estate_id } = request.all()
+    if (!id || !estate_id) {
+      throw new HttpException(WRONG_PARAMS, 400)
+    }
+
+    const estate = await EstateService.getById(estate_id ?? id)
+    if (!estate) {
+      throw new HttpException(NO_ESTATE_EXIST, 400)
+    }
+
+    if (estate.publish_status === PUBLISH_STATUS_APPROVED_BY_ADMIN) {
+      throw new HttpException(ERROR_PROPERTY_PUBLISHED_CAN_BE_EDITABLE, 400)
+    }
+
+    await next()
+  }
+}
+
+module.exports = EstateCanEdit
