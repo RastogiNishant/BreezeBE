760ac657559073ac8f86997ad453528558ddfa67
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-08-14 02:00:35 -0500

    income level using phrase

--- app/Validators/CreateTenantCertificate.js
+++ app/Validators/CreateTenantCertificate.js
@@ -6,7 +6,7 @@ class CreateTenantCertificate extends Base {
   static schema = () => {
     return yup.object().shape({
       city_id: yup.number().positive().required(),
-      income_level: yup.string().max(10).required(),
+      income_level: yup.string().max(100).required(),
       expired_at: yup.date().required(),
       file: yup.mixed(),
     })
