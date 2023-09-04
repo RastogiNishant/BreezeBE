1dc00fad9b2fd031f69a3170b57c9275e48732ec
Yong Yi Wong <yongyi.fullstack@gmail.com>
2023-09-04 03:37:13 -0500

    grouped estates for tenant according to build_id

--- app/Services/MatchService.js
+++ app/Services/MatchService.js
@@ -2726,6 +2726,10 @@ class MatchService {
       .clearSelect()
       .clearOrder()
       .select(Database.raw(`count(DISTINCT("estates"."id"))`))
+      .orderBy(
+        Database.raw(`case when build_id is null then estates.id else estates.build_id end`),
+        'DESC'
+      )
       .fetch()
   }
   /**
@@ -2736,7 +2740,6 @@ class MatchService {
     { buddy, like, dislike, knock, invite, visit, share, top, commit, final }
   ) {
     const defaultWhereIn = final ? [STATUS_DRAFT] : [STATUS_ACTIVE, STATUS_EXPIRE]
-
     const query = Estate.query()
       .select(
         Database.raw(
@@ -2749,6 +2752,10 @@ class MatchService {
       .withCount('notifications', function (n) {
         n.where('user_id', userId)
       })
+      .orderBy(
+        Database.raw(`case when build_id is null then estates.id else estates.build_id end`),
+        'DESC'
+      )
       .orderBy('_m.updated_at', 'DESC')
       .whereIn('estates.status', defaultWhereIn)
 
@@ -2768,6 +2775,11 @@ class MatchService {
       // All liked estates
       query
         .clearSelect()
+        .select(
+          Database.raw(
+            `distinct on (case when build_id is null then estates.id else estates.build_id end) build_id`
+          )
+        )
         .select('estates.*')
         .select('_m.updated_at', '_m.status_at')
         .select(Database.raw('"_l"."updated_at" as "action_at"'))
@@ -2785,6 +2797,11 @@ class MatchService {
       // All disliked estates
       query
         .clearSelect()
+        .select(
+          Database.raw(
+            `distinct on (case when build_id is null then estates.id else estates.build_id end) build_id`
+          )
+        )
         .select('estates.*')
         .select('_m.updated_at', '_m.status_at')
         .select(Database.raw('_d.created_at as action_at'))
@@ -2840,11 +2857,19 @@ class MatchService {
             })
         })
         .clearOrder()
+        .orderBy(
+          Database.raw(`case when build_id is null then estates.id else estates.build_id end`),
+          'DESC'
+        )
         .orderBy('_v.date', 'asc')
     } else if (share) {
       query
         .where({ '_m.share': true })
         .clearOrder()
+        .orderBy(
+          Database.raw(`case when build_id is null then estates.id else estates.build_id end`),
+          'DESC'
+        )
         .orderBy([
           { column: '_m.order_tenant', order: 'ASK' },
           { column: '_m.updated_at', order: 'DESC' },
@@ -2863,6 +2888,10 @@ class MatchService {
         .select('_u.avatar', '_u.firstname', '_u.secondname', '_u.sex')
         .where({ '_m.status': MATCH_STATUS_TOP, share: true })
         .clearOrder()
+        .orderBy(
+          Database.raw(`case when build_id is null then estates.id else estates.build_id end`),
+          'DESC'
+        )
         .orderBy([
           { column: '_m.order_tenant', order: 'ASK' },
           { column: '_m.updated_at', order: 'DESC' },
@@ -2924,6 +2953,7 @@ class MatchService {
       '_v.tenant_status AS visit_status',
       '_v.tenant_delay AS delay'
     )
+
     return query
   }
 
