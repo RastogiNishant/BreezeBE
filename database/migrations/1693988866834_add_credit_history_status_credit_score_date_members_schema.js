4e1a4aed3424a1a7e9da54ce0c82b8d2906d9e51
Justin Villocido <barudo@gmail.com>
2023-09-06 16:42:24 +0800

    use table instead of this

--- database/migrations/1693988866834_add_credit_history_status_credit_score_date_members_schema.js
+++ database/migrations/1693988866834_add_credit_history_status_credit_score_date_members_schema.js
@@ -7,18 +7,18 @@ class AddCreditHistoryStatusCreditScoreDateMembersSchema extends Schema {
   up() {
     this.table('members', (table) => {
       // alter table
-      this.integer('credit_history_status').comment(
-        '1 - no negative data, 2 - some negative data, 3 - enforceable claims'
-      )
-      this.date('credit_score_issued_at')
+      table
+        .integer('credit_history_status')
+        .comment('1 - no negative data, 2 - some negative data, 3 - enforceable claims')
+      table.date('credit_score_issued_at')
     })
   }
 
   down() {
     this.table('members', (table) => {
       // reverse alternations
-      this.dropColumn('credit_history_status')
-      this.dropColumn('credit_score_issued_at')
+      table.dropColumn('credit_history_status')
+      table.dropColumn('credit_score_issued_at')
     })
   }
 }
