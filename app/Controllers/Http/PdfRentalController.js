269d5f35f62816d55d62bd7f240c1648cdb0aa76
Sankalp Saxena <sankalpsaxenajava@gmail.com>
2023-09-22 07:22:45 +0530

    rental pdf changes for the backend code
    rental pdf changes for the backend code

--- app/Controllers/Http/PdfRentalController.js
+++ app/Controllers/Http/PdfRentalController.js
@@ -1,38 +1,17 @@
 'use strict'
-const fs = require('fs')
-const { PDFDocument, rgb } = require('pdf-lib');
+const axios = require("axios").default;
+const fs = require("fs");
 
 class PdfRentalController {
-  async generatePdf({ response }) {
+  async generatePdf({ request,auth,response }) {
 
-    const formPDFBytes = await fetch(process.env.RENT_URL).then(res => res.arrayBuffer())
-    const pdfDoc= await PDFDocument.load(formPDFBytes );
+   const { data } = await axios.get(process.env.RENT_URL, { responseType: "stream" });
 
-    const pdfFileName = 'Rental.pdf'
-    // Write the PDF bytes to a file
+   response.implicitEnd = false
+   response.header('Content-type', 'application/pdf')
 
-    const pdfFilePath = __dirname + '/' + pdfFileName
-    // Serialize the PDF to bytes
-    const pdfBytes = await pdfDoc.save()
+   data.pipe(response.response);
 
-   // Write the PDF bytes to a file
-    fs.writeFileSync(pdfFilePath, pdfBytes);
-    response.implicitEnd = false
-
-    // PDF Stream headers
-    response.header('Content-type', 'application/pdf')
-    response.header('Content-Disposition', 'inline; filename=' + pdfFileName)
-
-    // create a read stream from pdf file
-    const stream = fs.createReadStream(pdfFilePath)
-
-    // pipe the stream to the response object
-    stream.pipe(response.response)
-
-    // optional: log when the stream is finished
-    stream.on('end', () => {
-      console.log('PDF stream finished')
-    })
   }
 }
 
