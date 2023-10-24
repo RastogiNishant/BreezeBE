59a16b88f9b3df3e929b4e032371c03184ee5755
Justin Villocido <barudo@gmail.com>
2023-10-25 00:09:28 +0800

    add include_geography to query

--- app/Validators/TenantBuildFilter.js
+++ app/Validators/TenantBuildFilter.js
@@ -4,7 +4,8 @@ const yup = require('yup')
 class TenantBuildFilter {
   static schema = () =>
     yup.object().shape({
-      is_social: yup.boolean()
+      is_social: yup.boolean(),
+      include_geography: yup.boolean()
     })
 }
 
