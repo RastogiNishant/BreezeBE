2e0162a18055d9ffaf03ac3e9bd7cc25bb6fb539
Justin Villocido <barudo@gmail.com>
2023-11-03 14:57:19 +0800

    change estate.rooms_number to estate.floor

--- app/Services/EstateService.js
+++ app/Services/EstateService.js
@@ -1663,8 +1663,7 @@ class EstateService {
       estates = estates.filter(
         (estate) =>
           estate.floor === null ||
-          (estate.floor >= (tenant.floor_min || 0) &&
-            estate.rooms_number <= (tenant.floor_max || 20))
+          (estate.floor >= (tenant.floor_min || 0) && estate.floor <= (tenant.floor_max || 20))
       )
       if (process.env.DEV === 'true') {
         Logger.info(`filterEstates after floors ${estates?.length}`)
