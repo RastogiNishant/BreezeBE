2152d999ffc885498de2b26e33a6f98a85fe15a0
Justin Villocido <barudo@gmail.com>
2023-11-03 23:18:14 +0800

    add entrypoints for redo deactivation by income proofs

--- app/Classes/Queue.js
+++ app/Classes/Queue.js
@@ -14,7 +14,8 @@ const {
   SCHEDULED_9H_DAY_JOB,
   SCHEDULED_FRIDAY_JOB,
   SCHEDULED_MONTHLY_JOB,
-  SCHEDULED_EVERY_15MINUTE_NIGHT_JOB
+  SCHEDULED_EVERY_15MINUTE_NIGHT_JOB,
+  SCHEDULED_MONTHLY_AT_10TH_AND_AT_10AM
 } = require('../constants')
 const COMMON_QUEUE = 'common'
 
@@ -134,7 +135,7 @@ class QueueEngine {
         )
         .catch(Logger.error)
 
-      //scheduled at 12th of the month at 00:00
+      // scheduled at 12th of the month at 00:00
       this.commonQueue
         .add(
           SCHEDULED_MONTHLY_JOB,
@@ -142,6 +143,18 @@ class QueueEngine {
           { repeat: { cron: '0 0 12 * *' }, removeOnComplete: true, removeOnFail: true }
         )
         .catch(Logger.error)
+
+      this.commonQueue.add(
+        SCHEDULED_MONTHLY_AT_10TH_AND_AT_10AM,
+        {},
+        { repeat: { cron: '0 10 10 * *' }, removeOnComplete: true, removeOnFail: true }
+      )
+
+      this.commonQueue.add(
+        SCHEDULED_MONTHLY_AT_15TH_AND_AT_10AM,
+        {},
+        { repeat: { cron: '0 10 1t * *' }, removeOnComplete: true, removeOnFail: true }
+      )
 
       this.commonQueue.add(
         SCHEDULED_EVERY_15MINUTE_NIGHT_JOB,
@@ -171,6 +184,7 @@ class QueueEngine {
   async addJob(name, data, options = {}) {
     return this.commonQueue.add(name, data, Object.assign(defaultOptions, options))
   }
+
   async getJobById(id) {
     return this.commonQueue.getJob(id)
   }
--- app/Services/QueueService.js
+++ app/Services/QueueService.js
@@ -40,7 +40,9 @@ const {
   SCHEDULED_9H_DAY_JOB,
   SCHEDULED_MONTHLY_JOB,
   SCHEDULED_FOR_EVERY_MINUTE_ENDING_IN_3_JOB,
-  QUEUE_JOB_URGENT
+  QUEUE_JOB_URGENT,
+  SCHEDULED_MONTHLY_AT_10TH_AND_AT_10AM,
+  SCHEDULED_MONTHLY_AT_15TH_AND_AT_10AM
 } = require('../constants')
 const HttpException = require('../Exceptions/HttpException')
 
@@ -271,7 +273,7 @@ class QueueService {
    *
    */
   static async sendEveryMonth12AM() {
-    return Promise.all([wrapException(MemberService.handleOutdatedIncomeProofs)])
+    // return Promise.all([wrapException(MemberService.handleOutdatedIncomeProofs)])
   }
 
   static async addJobFetchPOI() {
@@ -301,6 +303,7 @@ class QueueService {
       Logger.error(`removeJobFetchPOI error ${e.message || e}`)
     }
   }
+
   /**
    *
    */
@@ -406,6 +409,12 @@ class QueueService {
             landlord_name: job.data.landlord_name,
             lang: job.data.lang
           })
+          break
+        case SCHEDULED_MONTHLY_AT_10TH_AND_AT_10AM:
+          require('./TenantService').scheduleProspectsForDeactivation()
+          break
+        case SCHEDULED_MONTHLY_AT_15TH_AND_AT_10AM:
+          require('./TenantService').handleProspectsScheduledForDeactivation()
           break
         default:
           console.log(`No job processor for: ${job.name}`)
