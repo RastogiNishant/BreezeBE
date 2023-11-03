1a67ee9dcc26959d404acfda502ec79546865955
Sankalp Saxena <sankalpsaxenajava@gmail.com>
2023-11-03 14:57:52 +0530

    likely have an effect on the notification with 56,1TUS_INVITE,
    likely have an effect on the notification              MATCH_STATUS_INVITE,

--- app/Services/NoticeService.js
+++ app/Services/NoticeService.js
@@ -639,15 +639,15 @@ class NoticeService {
     const groupMatches = groupBy(knockMatches, (match) => match.estate_id)
 
     let groupIdx = 0,
-      isNoticeCreated = false
+    // isNoticeCreated = false
     while (groupIdx < Object.keys(groupMatches).length) {
       const estate_id = Object.keys(groupMatches)[groupIdx]
       const freeTimeSlots = await require('./TimeSlotService').getFreeTimeslots(estate_id)
       if (!Object.keys(freeTimeSlots).length) {
         const estate = await require('./EstateService').getActiveById(estate_id)
 
-        if (!isNoticeCreated && estate) {
-          isNoticeCreated = true
+        if (estate) {
+          //isNoticeCreated = true
           notices.push({
             user_id: estate.user_id,
             type: NOTICE_TYPE_EXPIRED_SHOW_TIME_ID,
