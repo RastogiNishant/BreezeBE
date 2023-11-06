aa12c820401a130f157373c0ae8539e436ae09cb
Justin Villocido <barudo@gmail.com>
2023-11-06 15:42:56 +0800

    remove from queue monthly at 10

--- app/Classes/Queue.js
+++ app/Classes/Queue.js
@@ -13,9 +13,9 @@ const {
   SCHEDULED_13H_DAY_JOB,
   SCHEDULED_9H_DAY_JOB,
   SCHEDULED_FRIDAY_JOB,
-  SCHEDULED_MONTHLY_JOB,
   SCHEDULED_EVERY_15MINUTE_NIGHT_JOB,
-  SCHEDULED_MONTHLY_AT_10TH_AND_AT_10AM
+  SCHEDULED_MONTHLY_AT_10TH_AND_AT_10AM,
+  SCHEDULED_MONTHLY_AT_15TH_AND_AT_10AM
 } = require('../constants')
 const COMMON_QUEUE = 'common'
 
@@ -135,15 +135,6 @@ class QueueEngine {
         )
         .catch(Logger.error)
 
-      // scheduled at 12th of the month at 00:00
-      this.commonQueue
-        .add(
-          SCHEDULED_MONTHLY_JOB,
-          {},
-          { repeat: { cron: '0 0 12 * *' }, removeOnComplete: true, removeOnFail: true }
-        )
-        .catch(Logger.error)
-
       this.commonQueue.add(
         SCHEDULED_MONTHLY_AT_10TH_AND_AT_10AM,
         {},
@@ -153,7 +144,7 @@ class QueueEngine {
       this.commonQueue.add(
         SCHEDULED_MONTHLY_AT_15TH_AND_AT_10AM,
         {},
-        { repeat: { cron: '0 10 1t * *' }, removeOnComplete: true, removeOnFail: true }
+        { repeat: { cron: '0 10 15 * *' }, removeOnComplete: true, removeOnFail: true }
       )
 
       this.commonQueue.add(
