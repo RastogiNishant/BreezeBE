b131cdec8ff0e65568e80d0bc6f377a3e0087fb6
Justin Villocido <barudo@gmail.com>
2023-12-08 12:04:00 +0800

    update migrations to have 1 email sent or 2 emails sent

--- database/migrations/1701960216861_prepopulate_old_contact_requests_schema.js
+++ database/migrations/1701960216861_prepopulate_old_contact_requests_schema.js
@@ -9,13 +9,23 @@ const { DATE_FORMAT } = use('App/constants')
 class PrepopulateOldContactRequestsSchema extends Schema {
   up() {
     const yesterday = moment.utc(new Date()).add(-1, 'days').format(DATE_FORMAT)
+    const lastWeek = moment.utc(new Date()).add(-7, 'days').format(DATE_FORMAT)
     this.schedule(async (trx) => {
       // we let all old contact requests have one reminder sent
-      // so they'll have the next reminder next week
+      // so they'll have the next reminder on the following week
       await Database.raw(
         `update estate_sync_contact_requests 
-          set reminders_to_convert=1, last_reminder=NOW()
-          where created_at<='${yesterday}'`
+          set reminders_to_convert=1, last_reminder_at=NOW()
+          where created_at<='${yesterday}'
+          and user_id is null
+        `
+      )
+      // we let all older than last week contacts have 2 reminders sent
+      await Database.raw(
+        `update estate_sync_contact_requests
+          set reminders_to_convert=2, last_reminder_at='${lastWeek}'
+          where created_at<='${lastWeek}'
+        `
       )
     })
   }
